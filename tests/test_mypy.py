bb086aee49c90b96018a152d6e389521a2769dca
Brandon Elam Barker <beb82@cornell.edu>
2017-10-30 16:39:40 +0000

    added separate test for running mypy on tests

--- tests/test_mypy.py
+++ tests/test_mypy.py
@@ -1,19 +1,25 @@
 import os
+import glob
 import subprocess
 import unittest
 
 
 class MyPyTest(unittest.TestCase):
 
-    def test_run_mypy(self):
-        my_env = os.environ.copy()
-        pypath: str = my_env.get("PYTHONPATH", '')
-        browse_result: int = subprocess.call(["mypy", "-p", self.pkgname], env=os.environ, cwd=pypath)
+    def test_run_mypy_module(self):
+        browse_result: int = subprocess.call(["mypy", "-p", self.pkgname], env=os.environ, cwd=self.pypath)
         self.assertEqual(browse_result, 0, 'mypy on browse')
 
+    def test_run_mypy_tests(self):
+        for test_file in glob.iglob(f'{os.getcwd()}/tests/**/*.py', recursive=True):
+            test_result: int = subprocess.call(["mypy", test_file], env=os.environ, cwd=self.pypath)
+            self.assertEqual(test_result, 0, f'mypy on test {test_file}')
+
     def __init__(self, *args, **kwargs) -> None:
-        self.pkgname: str = "browse"
-        super(MyPyTest, self).__init__(*args, **kwargs)
+            self.pkgname: str = "browse"
+            super(MyPyTest, self).__init__(*args, **kwargs)
+            my_env = os.environ.copy()
+            self.pypath: str = my_env.get("PYTHONPATH", os.getcwd())
 
 
 if __name__ == '__main__':
