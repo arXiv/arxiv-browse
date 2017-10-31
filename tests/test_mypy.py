700f094eb6d3679e315b41ee3d89781b5fb1f57c
Brandon Elam Barker <beb82@cornell.edu>
2017-10-31 16:00:10 +0000

    don't require types from imported modules; all tests now pass

--- tests/test_mypy.py
+++ tests/test_mypy.py
@@ -2,24 +2,28 @@ import os
 import glob
 import subprocess
 import unittest
+from typing import List
 
 
 class MyPyTest(unittest.TestCase):
 
     def test_run_mypy_module(self):
-        browse_result: int = subprocess.call(["mypy", "-p", self.pkgname], env=os.environ, cwd=self.pypath)
+        mypy_call: List[str] = ["mypy"] + self.mypy_opts + ["-p", self.pkgname]
+        browse_result: int = subprocess.call(mypy_call, env=os.environ, cwd=self.pypath)
         self.assertEqual(browse_result, 0, 'mypy on browse')
 
     def test_run_mypy_tests(self):
         for test_file in glob.iglob(f'{os.getcwd()}/tests/**/*.py', recursive=True):
-            test_result: int = subprocess.call(["mypy", test_file], env=os.environ, cwd=self.pypath)
+            mypy_call: List[str] = ["mypy"] + self.mypy_opts + [test_file]
+            test_result: int = subprocess.call(mypy_call, env=os.environ, cwd=self.pypath)
             self.assertEqual(test_result, 0, f'mypy on test {test_file}')
 
     def __init__(self, *args, **kwargs) -> None:
-            self.pkgname: str = "browse"
-            super(MyPyTest, self).__init__(*args, **kwargs)
-            my_env = os.environ.copy()
-            self.pypath: str = my_env.get("PYTHONPATH", os.getcwd())
+        self.pkgname: str = "browse"
+        super(MyPyTest, self).__init__(*args, **kwargs)
+        my_env = os.environ.copy()
+        self.pypath: str = my_env.get("PYTHONPATH", os.getcwd())
+        self.mypy_opts: List[str] = ['--ignore-missing-imports']
 
 
 if __name__ == '__main__':
