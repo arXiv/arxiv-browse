2fba4fe768b94634e844f03d83fad26d8baea4f8
Martin Lessmeister <mhl10@cornell.edu>
2017-11-09 13:30:13 -0500

    Add docstrings and fix formatting.

--- tests/test_mypy.py
+++ tests/test_mypy.py
@@ -1,3 +1,4 @@
+"""Tests using mypy."""
 import os
 import glob
 import subprocess
@@ -6,21 +7,26 @@ from typing import List
 
 
 class MyPyTest(unittest.TestCase):
+    """Class for testing modules with mypy."""
 
     def test_run_mypy_module(self):
-        """Run mypy on all module sources"""
+        """Run mypy on all module sources."""
         mypy_call: List[str] = ["mypy"] + self.mypy_opts + ["-p", self.pkgname]
-        browse_result: int = subprocess.call(mypy_call, env=os.environ, cwd=self.pypath)
+        browse_result: int = subprocess.call(
+            mypy_call, env=os.environ, cwd=self.pypath)
         self.assertEqual(browse_result, 0, 'mypy on browse')
 
     def test_run_mypy_tests(self):
-        """Run mypy on all tests in module under the tests directory"""
-        for test_file in glob.iglob(f'{os.getcwd()}/tests/**/*.py', recursive=True):
+        """Run mypy on all tests in module under the tests directory."""
+        for test_file in glob.iglob(f'{os.getcwd()}/tests/**/*.py',
+                                    recursive=True):
             mypy_call: List[str] = ["mypy"] + self.mypy_opts + [test_file]
-            test_result: int = subprocess.call(mypy_call, env=os.environ, cwd=self.pypath)
+            test_result: int = subprocess.call(
+                mypy_call, env=os.environ, cwd=self.pypath)
             self.assertEqual(test_result, 0, f'mypy on test {test_file}')
 
     def __init__(self, *args, **kwargs) -> None:
+        """Set up some common variables."""
         self.pkgname: str = "browse"
         super(MyPyTest, self).__init__(*args, **kwargs)
         my_env = os.environ.copy()
