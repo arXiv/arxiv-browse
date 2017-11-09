d08a220b62c37f10f7200058e8e15734a99e0755
Martin Lessmeister <mhl10@cornell.edu>
2017-11-09 09:03:30 -0500

    Fix indentation

--- browse/services/database/__init__.py
+++ browse/services/database/__init__.py
@@ -13,16 +13,24 @@ def get_institution(ip: str):
     """Get institution label from IP address."""
     decimal_ip = int(ipaddress.ip_address(ip))
     try:
-        stmt = db.session.query(MemberInstitution.label,
-                                func.sum(MemberInstitutionIP.exclude).
-                                label("exclusions")). \
-                                join(MemberInstitutionIP).filter(
-                                    MemberInstitutionIP.start <= decimal_ip,
-                                    MemberInstitutionIP.end >= decimal_ip). \
-                                group_by(MemberInstitution.label). \
-                                subquery()
-        return db.session.query(stmt.c.label). \
-            filter(stmt.c.exclusions == 0).one().label
+        stmt = (
+            db.session.query(
+                MemberInstitution.label,
+                func.sum(MemberInstitutionIP.exclude).label("exclusions")
+            ).
+            join(MemberInstitutionIP).
+            filter(
+                MemberInstitutionIP.start <= decimal_ip,
+                MemberInstitutionIP.end >= decimal_ip
+            ).
+            group_by(MemberInstitution.label).
+            subquery()
+        )
+
+        return (
+                    db.session.query(stmt.c.label).
+                    filter(stmt.c.exclusions == 0).one().label
+               )
     except NoResultFound:
         return None
     except SQLAlchemyError as e:
