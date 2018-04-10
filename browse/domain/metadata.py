815448829cdd86ce5d6f79c3b5c716f68b94ca76
Martin Lessmeister <mlessmeister@Martins-MacBook-Pro.local>
2018-04-09 21:34:25 -0400

    Update metadata domain classes

--- browse/domain/metadata.py
+++ browse/domain/metadata.py
@@ -1,6 +1,6 @@
 """Representations of arXiv document metadata."""
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Optional
 from datetime import datetime
 
 
@@ -9,9 +9,9 @@ class License():
     """Represents an arXiv article license."""
 
     """Name of the license."""
-    name: str = field(default_factory=str)
+    name: Optional[str] = None
     """URI of the license."""
-    uri: str = field(default_factory=str)
+    uri: Optional[str] = None
 
 
 @dataclass
@@ -25,7 +25,7 @@ class SourceType():
 
 
 @dataclass
-class Submitter:
+class Submitter():
     """Represents the person who submitted an arXiv article."""
 
     """Full name."""
@@ -39,14 +39,13 @@ class Submitter:
 class VersionEntry():
     """Represents a single arXiv article version history entry."""
 
+    version: int
+
     """Raw history entry, e.g. as parsed from .abs file."""
-    raw: str = field(default_factory=str)
+    raw: str
 
     """Date for the entry."""
-    date: datetime = field(default_factory=datetime)
-
-    """Submitter associated with this entry."""
-    submitter: Submitter = field(default_factory=Submitter)
+    submitted_date: datetime
 
     """Size of the article source, in kilobytes."""
     size_kilobytes: int = 0
@@ -67,8 +66,8 @@ class AuthorList():
 class DocMetadata():
     """Class for representing the core arXiv document metadata."""
 
-    """arXiv paper identifier.z"""
-    """TODO: strict typing"""
+    """arXiv paper identifier"""
+    """TODO: stricter typing?"""
     paper_id: str = field(default_factory=str)
 
     """Title of the article."""
@@ -84,29 +83,36 @@ class DocMetadata():
     submitter: Submitter = field(default_factory=Submitter)
 
     """Article classification."""
-    article_classification: str = field(default_factory=str)
+    categories: str = field(default_factory=str)
 
     """Journal reference."""
-    journal_ref: str = field(default_factory=str)
+    journal_ref: Optional[str] = None
 
     """Report number."""
-    report_num: str = field(default_factory=str)
+    report_num: Optional[str] = None
 
     """Digital Object Identifier (DOI)."""
-    doi: str = field(default_factory=str)
+    doi: Optional[str] = None
 
     """Association for Computing Machinery (ACM) classification(s)."""
-    acm_class: str = field(default_factory=str)
+    acm_class: Optional[str] = None
 
     """American Mathematical Society Mathematics Subject (MSC)
        classification(s)."""
-    msc_class: str = field(default_factory=str)
+    msc_class: Optional[str] = None
 
     """License associated with the article."""
-    license: License = field(default_factory=License)
+    # license: License = field(default_factory=License)
+    license: Optional[str] = None
+
+    """Proxy."""
+    proxy: Optional[str] = None
 
     """Submitter- and/or administrator-provided comments about the article."""
-    comments: str = field(default_factory=str)
+    comments: Optional[str] = None
 
     """Version history, consisting of at least one version history entry."""
     version_history: List[VersionEntry] = field(default_factory=list)
+
+    """Version of this paper."""
+    version: int = 1
