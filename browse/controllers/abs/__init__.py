42e540c695a20337b7f5cbc96af459e5d395d05e
Martin Lessmeister <mhl10@cornell.edu>
2018-05-29 10:26:14 -0400

    minor refactor

--- browse/controllers/abs/__init__.py
+++ browse/controllers/abs/__init__.py
@@ -5,7 +5,7 @@ The primary entrypoint to this module is :func:`.get_abs_page`, which handles
 GET requests to the abs endpoint.
 """
 
-from typing import Tuple, Dict, Any
+from typing import Tuple, Dict, Any, Optional, Str
 from arxiv import status, taxonomy
 from browse.services.document import metadata
 from browse.services.document.metadata import AbsException,\
@@ -44,35 +44,17 @@ def get_abs_page(arxiv_id: str, request_params: MultiDict) -> Response:
 
     """
     response_data = {}  # type: Dict[str, Any]
-    # TODO: refactor
     try:
         arxiv_identifier = Identifier(arxiv_id=arxiv_id)
-        if arxiv_identifier and arxiv_identifier.ids != arxiv_identifier.id \
-           and arxiv_identifier.ids != arxiv_identifier.idv:
-            redirect_url = arxiv_identifier.idv \
-                          if arxiv_identifier.has_version \
-                          else arxiv_identifier.id
+        redirect_url = _check_supplied_identifier(arxiv_identifier)
+        if redirect_url:
             return {},\
-                status.HTTP_301_MOVED_PERMANENTLY,\
-                {'Location': url_for('browse.abstract', arxiv_id=redirect_url)}
+                   status.HTTP_301_MOVED_PERMANENTLY,\
+                   {'Location': redirect_url}
 
-        abs_meta = metadata.get_abs(arxiv_id)
-        response_data['abs_meta'] = abs_meta
-        if 'context' in request_params\
-           and (request_params['context'] in taxonomy.CATEGORIES
-                or request_params['context'] in taxonomy.ARCHIVES
-                or request_params['context'] == 'arxiv'):
-            if request_params['context'] == 'arxiv':
-                response_data['browse_context_next_id'] = \
-                    metadata.get_next_id(arxiv_identifier)
-                response_data['browse_context_previous_id'] = \
-                    metadata.get_previous_id(arxiv_identifier)
-            response_data['browse_context'] = request_params['context']
-        elif arxiv_identifier.is_old_id:
-            response_data['browse_context_next_id'] = \
-                metadata.get_next_id(arxiv_identifier)
-            response_data['browse_context_previous_id'] = \
-                metadata.get_previous_id(arxiv_identifier)
+        response_data['abs_meta'] = metadata.get_abs(arxiv_id)
+        _check_context(request_params, response_data, arxiv_identifier)
+
     except AbsNotFoundException:
         if arxiv_identifier.is_old_id and arxiv_identifier.archive \
            in taxonomy.ARCHIVES:
@@ -107,6 +89,60 @@ def get_abs_page(arxiv_id: str, request_params: MultiDict) -> Response:
     metadata.get_next_id(identifier=arxiv_identifier)
     return response_data, status.HTTP_200_OK, {}
 
-# def identifier_check(identifier: Identifier) -> None:
-#     """Check supplied identifier."""
-#     pass
+
+def _check_supplied_identifier(arxiv_identifier: Identifier) -> Optional(Str):
+    """
+    Provide redirect URL if supplied ID does not match parsed ID.
+
+    Parameters
+    ----------
+    arxiv_identier: :class:`Identifier`
+
+    Returns
+    -------
+    redirect_url: str
+        The redirect URL with the canonical arXiv identifier.
+
+    """
+    if arxiv_identifier and arxiv_identifier.ids != arxiv_identifier.id and \
+            arxiv_identifier.ids != arxiv_identifier.idv:
+        redirect_url = url_for('browse.abstract',
+                               arxiv_id=arxiv_identifier.idv
+                               if arxiv_identifier.has_version
+                               else arxiv_identifier.id)
+        return redirect_url
+    return None
+
+
+def _check_context(request_params: MultiDict,
+                   response_data: Dict,
+                   arxiv_identifier: Identifier) -> None:
+    """
+    Check context in request parameters and update response accordingly.
+
+    Parameters
+    ----------
+    request_params: MultiDict
+    response_data: dict
+    arxiv_identifier: :class:`Identifier`
+
+    Returns
+    -------
+    None
+
+    """
+    if 'context' in request_params\
+       and (request_params['context'] in taxonomy.CATEGORIES
+            or request_params['context'] in taxonomy.ARCHIVES
+            or request_params['context'] == 'arxiv'):
+        if request_params['context'] == 'arxiv':
+            response_data['browse_context_next_id'] = \
+                metadata.get_next_id(arxiv_identifier)
+            response_data['browse_context_previous_id'] = \
+                metadata.get_previous_id(arxiv_identifier)
+        response_data['browse_context'] = request_params['context']
+    elif arxiv_identifier.is_old_id:
+        response_data['browse_context_next_id'] = \
+            metadata.get_next_id(arxiv_identifier)
+        response_data['browse_context_previous_id'] = \
+            metadata.get_previous_id(arxiv_identifier)
