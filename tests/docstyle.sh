658e18c7766b18754b6ffcf212bf1129b103866e
Martin Lessmeister <mlessmeister@Martins-MacBook-Pro.local>
2018-06-22 16:05:30 -0400

    Add docstyle and lint bash scripts

---
+++ tests/docstyle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pipenv run pydocstyle --convention=numpy --add-ignore=D401
+PYDOCSTYLE_STATUS=$?
+if [ $PYDOCSTYLE_STATUS -ne 0 ]; then PYDOCSTYLE_STATE="failure" && echo "pydocstyle failed"; else PYDOCSTYLE_STATE="success" &&  echo "pydocstyle passed"; fi
+curl -u $USERNAME:$GITHUB_TOKEN \
+    -d '{"state": "'$PYDOCSTYLE_STATE'", "target_url": "https://travis-ci.org/'$TRAVIS_REPO_SLUG'/builds/'$TRAVIS_BUILD_ID'", "description": "", "context": "code-quality/pydocstyle"}' \
+    -XPOST https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$SHA \
+    > /dev/null 2>&1
