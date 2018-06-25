5c38ebf7c006a02eb3e88e368fef217b5df10c46
Brian D. Caruso <bdc34@cornell.edu>
2018-06-25 15:29:01 -0400

    Fixes failing test with message about DB.
    The test was initializing an in memory DB per test method, not
    per test class.

--- tests/test_database_service.py
+++ tests/test_database_service.py
@@ -10,10 +10,12 @@ DATABASE_URL = 'sqlite:///:memory:'
 class TestGetInstitution(TestCase):
     """:func:`.get_institution` gets an institution label for an IP address."""
 
-    def setUp(self) -> None:
+    @classmethod
+    def setUpClass(cls) -> None:
         """Initialize a database session with in-memory SQLite."""
         from browse.services import database
-        self.database_service = database
+
+        cls.database_service = database
         mock_app = mock.MagicMock()
         mock_app.config = {'SQLALCHEMY_DATABASE_URI': DATABASE_URL,
                            'SQLALCHEMY_TRACK_MODIFICATIONS': False}
@@ -24,6 +26,8 @@ class TestGetInstitution(TestCase):
         database.db.init_app(mock_app)
         database.db.app = mock_app
         database.db.create_all()
+        database.db.session.commit()
+
         inst_cornell = database.models.MemberInstitution(
             id=1,
             name='Cornell University',
@@ -40,8 +44,8 @@ class TestGetInstitution(TestCase):
         )
         database.db.session.add(inst_cornell_ip)
 
-        inst_cornell_ip_exclude = database.models. \
-            MemberInstitutionIP(
+        inst_cornell_ip_exclude = \
+            database.models.MemberInstitutionIP(
                 id=2,
                 sid=inst_cornell.id,
                 start=2152991233,  # 128.84.10.1
@@ -65,37 +69,46 @@ class TestGetInstitution(TestCase):
             exclude=0
         )
         database.db.session.add(inst_other_ip)
+        database.db.session.commit()
+
         sql_dir = test_path_of('data/db/sql')
         sql_files: List[str] = glob.glob(f'{sql_dir}/*.sql')
         execute_sql_files(sql_files, database.db.engine)
+        database.db.session.commit()
 
     def test_get_institution_returns_a_label(self) -> None:
         """If IP address matches an institution, a label is returned."""
-        label = self.database_service.get_institution('128.84.0.0')
+        label = TestGetInstitution.database_service.get_institution(
+            '128.84.0.0')
         self.assertEqual(label, 'Cornell University',
                          'Institution label returned for IP at end of range')
-        label = self.database_service.get_institution('128.84.255.255')
+        label = TestGetInstitution.database_service.get_institution(
+            '128.84.255.255')
         self.assertEqual(label, 'Cornell University',
                          'Institution label returned for IP at end of range')
 
-        label = self.database_service.get_institution('128.84.12.34')
+        label = TestGetInstitution.database_service.get_institution(
+            '128.84.12.34')
         self.assertEqual(label, 'Cornell University',
                          'Institution label returned for IP within range')
-        label = self.database_service.get_institution('128.85.12.34')
+        label = TestGetInstitution.database_service.get_institution(
+            '128.85.12.34')
         self.assertIsNone(
             label, 'No institution label returned for non-matching IP')
-        label = self.database_service.get_institution('128.84.10.1')
+        label = TestGetInstitution.database_service.get_institution(
+            '128.84.10.1')
         self.assertIsNone(
             label, 'No institution label returned for excluded IP')
 
-        label = self.database_service.get_institution('128.84.10.5')
+        label = TestGetInstitution.database_service.get_institution(
+            '128.84.10.5')
         self.assertEqual(
             label, 'Other University',
             'Institution label returned for IP excluded '
             'by one institution but included by another')
 
         with self.assertRaises(ValueError) as context:
-            self.database_service.get_institution('notanip')
+            TestGetInstitution.database_service.get_institution('notanip')
 
         self.assertIn(
             'does not appear to be an IPv4 or IPv6 address',
@@ -109,8 +122,8 @@ class TestGetInstitution(TestCase):
             doc_sql_file,
             '''INSERT INTO `arXiv_trackback_pings`'''
         )
-        count_from_db: int = self.database_service.count_all_trackback_pings()
-        count_from_db_list: int = self.database_service\
+        count_from_db: int = TestGetInstitution.database_service.count_all_trackback_pings()
+        count_from_db_list: int = TestGetInstitution.database_service\
             .get_all_trackback_pings().__len__()
 
         if count_from_file is not None:
@@ -132,9 +145,9 @@ class TestGetInstitution(TestCase):
     def test_trackback_pings(self) -> None:
         """Test if trackback pings for specific paper are counted"""
         test_paper_id = '0808.4142'
-        count_from_db: int = self.database_service.\
+        count_from_db: int = TestGetInstitution.database_service.\
             count_trackback_pings(test_paper_id)
-        count_from_db_list: int = self.database_service. \
+        count_from_db_list: int = TestGetInstitution.database_service. \
             get_trackback_pings(test_paper_id).__len__()
         self.assertEqual(
             count_from_db, 8,
@@ -145,8 +158,8 @@ class TestGetInstitution(TestCase):
             f'Correct count of pings returned for paper {test_paper_id}'
         )
 
-
-def tearDown(self) -> None:
+    @classmethod
+    def tearDownClass(cls) -> None:
         """Close the database session and drop all tables."""
-        database.db.session.remove()
-        database.db.drop_all()
+        cls.database_service.db.session.remove()
+        cls.database_service.db.drop_all()
