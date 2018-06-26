33a0dbf187ea12d66b1a34f6784892d3e553beae
Brian D. Caruso <bdc34@cornell.edu>
2018-06-26 15:24:41 -0400

    More reverting
    Git did not revert everthing in the previous commit.

--- browse/factory.py
+++ browse/factory.py
@@ -4,7 +4,6 @@ from functools import partial
 from arxiv.base import Base
 from flask import Flask, url_for
 
-from browse.domain.clickthrough import create_ct_url
 from browse.routes import ui
 from browse.services.database import models
 
@@ -19,5 +18,4 @@ def create_web_app() -> Flask:
     Base(app)
     app.register_blueprint(ui.blueprint)
 
-    app.jinja_env.filters['clickthrough_url_for'] = partial(create_ct_url, app.config.get('SECRET_KEY'), url_for)
     return app
