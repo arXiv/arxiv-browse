7fdfedc046462779d8592af078af1139f1bc05a9
Brian D. Caruso <bdc34@cornell.edu>
2018-07-13 15:46:28 -0400

    Adding stub routes to ui.py and adding more url_for() in templates
    ARXIVNG-926

--- browse/routes/ui.py
+++ browse/routes/ui.py
@@ -54,7 +54,6 @@ def abstract(arxiv_id: str) -> Union[str, Response]:
 
 @blueprint.route('/trackback/', methods=['GET'], defaults={'arxiv_id': ''})
 @blueprint.route('/trackback/<path:arxiv_id>', methods=['GET', 'POST'])
-
 def trackback(arxiv_id: str) -> Union[str, Response]:
     """Route to define new trackbacks for papers."""
     # TODO implement
@@ -69,3 +68,87 @@ def clickthrough():
         return redirect(request.args.get('url'))
     else:
         raise NotFound()
+
+# Satic resources (not sure how to do these in NG):
+# @blueprint.route(//static.arxiv.org/css/arXiv.css?v=20170424)
+# @blueprint.route(/favicon.ico)
+# @blueprint.route(http://arxiv.org/)
+
+# Not sure how to do these cross repo links,
+# Will talk to Erick or look into arxiv-base to see.
+# @blueprint.route(/IgnoreMe)
+# @blueprint.route(/find)
+# @blueprint.route(/form)
+# @blueprint.route(/help)
+# @blueprint.route(/help/arxiv_identifier)
+# @blueprint.route(/help/arxiv_identifier)
+# @blueprint.route(/help/mathjax/)
+# @blueprint.route(/help/trackback/)
+# @blueprint.route(/help/contact)
+# @blueprint.route(/help/social_bookmarking)
+# @blueprint.route(/search)
+# @blueprint.route(/user/login)
+
+
+@blueprint.route('/list/<context>/<subcontext>')
+def list_articles(current_context: str, yymm: str):
+    # context might be a context or an archive
+    # subcontext should be 'recent' 'new' or a string of format yymm
+    raise InternalServerError('Not yet implemented')
+
+
+@blueprint.route('/format/<arxiv_id>')
+def format(arxiv_id: str):
+    raise InternalServerError('Not yet implemented')
+
+
+@blueprint.route('/pdf/<arxiv_id>')
+def pdf(arxiv_id: str):
+    raise InternalServerError('Not yet implemented')
+
+
+@blueprint.route('/div/<arxiv_id>')
+def div(arxiv_id: str):
+    raise InternalServerError('Not yet implemented')
+
+
+@blueprint.route('/html/<arxiv_id>')
+def html(arxiv_id: str):
+    raise InternalServerError('Not yet implemented')
+
+
+@blueprint.route('/ps/<arxiv_id>')
+def ps(arxiv_id: str):
+    raise InternalServerError('Not yet implemented')
+
+
+@blueprint.route('/src/<arxiv_id>/anc', defaults={'file_name': None})
+@blueprint.route('/src/<arxiv_id>/anc/<path:file_name>')
+def src(arxiv_id: str, file_name: str):
+    raise InternalServerError('Not Yet Implemented')
+
+
+@blueprint.route('/tb/<path:arxiv_id>')
+def tb(arxiv_id: str):
+    raise InternalServerError('Not yet implemented')
+
+
+@blueprint.route('/show-email/<path:show_email_hash>/<path:arxiv_id>')
+def show_email(show_email_hash: str, arxiv_id: str):
+    raise InternalServerError('Not Yet Implemented')
+
+
+# Maybe auth protected URL in arxiv-browse? ('will the auth service allow paths not defined in it's repo to be protected?')
+@blueprint.route('/auth/show-endorsers/<path:arxiv_id>')
+def show_endorsers(arxiv_id: str):
+    raise InternalServerError('Not yet implemented')
+
+
+@blueprint.route('/refs/<path:arxiv_id>')
+def refs(arxiv_id: str):
+    raise InternalServerError('Not yet implemented')
+
+
+@blueprint.route('/cits/<path:arxiv_id>')
+def cits(arxiv_id: str):
+    raise InternalServerError('Not yet implemented')
