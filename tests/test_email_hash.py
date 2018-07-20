112ac265b6a6d1356e50f19725fad5d599d69727
Brian D. Caruso <bdc34@cornell.edu>
2018-07-20 11:58:40 -0400

    Adding test for email hash

---
+++ tests/test_email_hash.py
@@ -0,0 +1,20 @@
+import unittest
+
+from hypothesis import given
+from hypothesis.strategies import text
+
+from browse.services.util.email import generate_show_email_hash
+
+
+class TestEmailHAsh(unittest.TestCase):
+
+    @given(text(), text(), text())
+    def test_fuzz_email_hash(self, s, v, x):
+        h = generate_show_email_hash(s, v)
+        self.assertEqual(h, generate_show_email_hash(s, v))
+
+        if h is not None:
+            if v != s:
+                self.assertNotEqual(h, generate_show_email_hash(v, s))
+            self.assertNotEqual(h, generate_show_email_hash(s, x))
+            self.assertNotEqual(h, generate_show_email_hash(x, v))
