6933fbfe96d1b4e4ca721dc749b3db9f81fcb938
Brandon Elam Barker <beb82@cornell.edu>
2018-07-25 20:54:56 +0000

    improving UX of test type-error reporting

--- tests/test_typings.py
+++ tests/test_typings.py
@@ -22,6 +22,10 @@ class MyPyTest(TestCase):
         """Run mypy on all tests in module under the tests directory."""
 
         test_failures: int = 0
+        test_out_file_name = "mypy_test_errors.txt"
+        if os.path.exists(test_out_file_name):
+            os.remove(test_out_file_name)
+        test_out_file = open(test_out_file_name, "w+")
         for test_file in glob.iglob(f'{os.getcwd()}/tests/**/*.py',
                                     recursive=True):
             mypy_call: List[str] = ["mypy"] + self.mypy_opts + [test_file]
@@ -31,10 +35,15 @@ class MyPyTest(TestCase):
                     mypy_call, env=os.environ, cwd=self.pypath)
             except CalledProcessError as ex:
                 test_result = ex.output.decode()
-            print(test_result)
             test_failures += len(test_result.splitlines())
+            test_out_file.writelines(test_result)
+        test_out_file.close()
         # Choose arbitrary number of test errors to tolerate:
         # TODO: probably need to bring this down slightly
+        print('***** * * *   Current number of mypy errors in tests:' 
+              f'{test_failures}  * * * *****\n'
+              f'***** * * *   See {test_out_file_name} for error log  * * * *****'
+              )
         self.assertLessEqual(test_failures, 150, f'mypy on test {test_file}')
 
     def __init__(self, *args: str, **kwargs: Dict) -> None:
