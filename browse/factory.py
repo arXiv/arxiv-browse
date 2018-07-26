ad3871121bc7843c11088d42f10c4ac2d16ea1f5
Martin Lessmeister <mhl10@cornell.edu>
2018-07-26 16:58:59 -0400

    Add DOI filter

--- browse/factory.py
+++ browse/factory.py
@@ -1,14 +1,13 @@
 """Application factory for browse service components."""
 from functools import partial
-from inspect import getmembers, isfunction
 
 from arxiv.base import Base
-from flask import Flask, url_for
+from flask import Flask
 
 from browse.routes import ui
 from browse.services.database import models
 from browse.services.util.email import generate_show_email_hash
-from browse.filters import abstract_filter, url_filter
+from browse.filters import abstract_filter, doi_filter, url_filter
 
 
 def create_web_app() -> Flask:
@@ -26,6 +25,7 @@ def create_web_app() -> Flask:
                 secret=app.config.get('SHOW_EMAIL_SECRET'))
 
     app.jinja_env.filters['abstract_filter'] = abstract_filter
+    app.jinja_env.filters['doi_filter'] = doi_filter
     app.jinja_env.filters['url_filter'] = url_filter
 
     return app
