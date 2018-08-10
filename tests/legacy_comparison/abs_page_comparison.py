294b3f3e60ed89de1e4e24c8a1ac59d63570c5ca
Brian D. Caruso <bdc34@cornell.edu>
2018-08-10 15:52:04 -0400

    Refining types for test of abs against legacy pages.
    ARXIVNG-1051

--- tests/legacy_comparison/abs_page_comparison.py
+++ tests/legacy_comparison/abs_page_comparison.py
@@ -3,15 +3,15 @@ import sys
 # BDC34: some how I need this under pipenv to get to browse, not sure why
 sys.path.append('')
 
-from tests.legacy_comparison.comparison_types import res_comparison_fn, text_comparison_fn,html_comparison_fn
+from tests.legacy_comparison.comparison_types import res_comparison_fn, text_comparison_fn, html_comparison_fn, \
+    res_arg_dict, text_arg_dict, html_arg_dict
 from tests.legacy_comparison.response_comparisons import compare_status
 
 import os
 from functools import partial
 from multiprocessing import Pool
 import requests
-from typing import Iterator, Callable, List, Any
-from mypy_extensions import TypedDict
+from typing import Iterator, List, Callable, TypeVar
 
 from browse.services.document.metadata import AbsMetaSession
 from tests import path_of_for_test
@@ -26,7 +26,7 @@ text_comparisons: List[text_comparison_fn] = []
 html_comparisons: List[html_comparison_fn] = []
 
 
-def paperid_iterator(path) -> Iterator[str]:
+def paperid_iterator(path: str) -> List[str]:
     """Return an iterator of paperId strings for all abs found below path."""
     ids = []
     for (dir_name, subdir_list, file_list) in os.walk(path):
@@ -48,63 +48,64 @@ ng_abs_base_url = 'http://localhost:5000/abs/'
 legacy_abs_base_url = 'https://beta.arxiv.org/abs/'
 
 
-def fetch_and_compare_abs(compare_res_fn, paperid):
+T = TypeVar('T')
+
+
+def fetch_and_compare_abs(compare_res_fn: Callable[[res_arg_dict], List[T]], paperid: str) -> List[T]:
     ng_url = ng_abs_base_url + paperid
     legacy_url = legacy_abs_base_url + paperid
-    return compare_res_fn(ng_url=ng_url, legacy_url=legacy_url,
-                          ng_res=requests.get(ng_url), legacy_res=requests.get(legacy_url),
-                          paperid=paperid)
+
+    res_dict: res_arg_dict = {'ng_url': ng_url,
+                              'legacy_url': legacy_url,
+                              'ng_res': requests.get(ng_url),
+                              'legacy_res': requests.get(legacy_url),
+                              'paperid': paperid}
+
+    return compare_res_fn(res_dict)
 
 
-def run_compare_response(ng_url: str=None,
-                         legacy_url: str=None,
-                         ng_res: requests.Response =None,
-                         legacy_res: requests.Response =None,
-                         paperid: str=None):
-    # TODO run all response_comparisons
-    def call_it(fn):
+def run_compare_response(res_args: res_arg_dict) -> List[str]:
+
+    text_dict: text_arg_dict = {**res_args, **{'ng_text': res_args['ng_res'].text,
+                                               'legacy_text': res_args['legacy_res'].text}}
+
+    def call_it(fn: Callable[[text_arg_dict], str]) -> str:
         try:
-            return fn(ng_url=ng_url, legacy_url=legacy_url, ng_res=ng_res, legacy_res=legacy_res, paperid=paperid)
+            return fn(text_dict)
         except Exception as ex:
-            return ex
+            return str(ex)
 
     return list(filter(None, itertools.chain(
-        map(call_it, res_comparisons),
-        run_compare_text(ng_url=ng_url, legacy_url=legacy_url, ng_text=ng_res.text,
-                         legacy_text=legacy_res.text, paperid=paperid))))
+        map(call_it, res_comparisons), run_compare_text(text_dict))))
 
 
-def run_compare_text(ng_url: str=None, legacy_url: str=None,
-                     ng_text: str=None, legacy_text: str=None,
-                     paperid: str=None):
-    def call_it(fn):
+def run_compare_text(text_args: text_arg_dict) -> Iterator[str]:
+
+    html_dict: html_arg_dict = {**text_args, **{'ng_html': BeautifulSoup(text_args['ng_text'], 'html.parser'),
+                                                'legacy_html': BeautifulSoup(text_args['legacy_text'], 'html.parser')}}
+
+    def call_it(fn: Callable[[html_arg_dict], str]) -> str:
         try:
-            return fn(ng_url=ng_url, legacy_url=legacy_url, ng_text=ng_text, legacy_text=legacy_text, paperid=paperid)
+            return fn(html_dict)
         except Exception as ex:
-            return ex
+            return str(ex)
 
     return filter(None, itertools.chain(
-        map(call_it, text_comparisons),
-        run_compare_html(ng_url=ng_url, legacy_url=legacy_url,
-                         ng_html=BeautifulSoup(ng_text, 'html.parser'),
-                         legacy_html=BeautifulSoup(legacy_text, 'html.parser'),
-                         paperid=paperid)))
+        map(call_it, text_comparisons), run_compare_html(html_dict)))
 
 
-def run_compare_html(ng_url: str=None, legacy_url: str=None,
-                     ng_html: BeautifulSoup=None, legacy_html: BeautifulSoup=None,
-                     paperid: str=None):
-    def call_it(fn):
+def run_compare_html(html_args: html_arg_dict) -> Iterator[str]:
+    def call_it(fn: Callable[[html_arg_dict], str]) -> str:
         try:
-            return fn(ng_url=ng_url, legacy_url=legacy_url, ng_html=ng_html, legacy_html=legacy_html, paperid=paperid)
+            return fn(html_args)
         except Exception as ex:
-            return ex
+            return str(ex)
 
-    return itertools.filterfalse(lambda x: x, map(call_it, html_comparisons))
+    return filter(None, map(call_it, html_comparisons))
 
 
-#papers =  ['0704.0001', '0704.0600']
-papers = paperid_iterator(ABS_FILES)
+papers = ['0704.0001', '0704.0600']
+# papers = paperid_iterator(ABS_FILES)
 with Pool(10) as p:
     compare = partial(fetch_and_compare_abs, run_compare_response)
     results = p.imap(compare, papers)
--- tests/legacy_comparison/response_comparisons.py
+++ tests/legacy_comparison/response_comparisons.py
@@ -1,12 +1,12 @@
 import requests
 
-def compare_status(ng_url: str=None,
-                   legacy_url: str=None,
-                   ng_res: requests.Response =None,
-                   legacy_res: requests.Response =None,
-                   paperid: str=None):
-    if ng_res.status_code ==200  and legacy_res.status_code == 200:
-        return f'200 HTTP status for both {ng_url} and {legacy_url}'
-    else:
-        return f'HTTP status for {ng_url} was {ng_res.status_code} and for {legacy_url} was {legacy_res.status_code}'
+from tests.legacy_comparison.comparison_types import res_arg_dict
 
+
+def compare_status(res_arg: res_arg_dict) -> str:
+    if res_arg['ng_res'].status_code == 200 and res_arg['legacy_res'].status_code == 200:
+
+        return f'200 HTTP status for both {res_arg["ng_url"]} and {res_arg["legacy_url"]}'
+    else:
+        return f'HTTP status for {res_arg["ng_url"]} was {res_arg["ng_res"].status_code} '\
+               f'and for {res_arg["legacy_url"]} was {res_arg["legacy_res"].status_code}'
