a7a3209609da3d6deb3269f55cfcc1b3ed8410aa
Brian D. Caruso <bdc34@cornell.edu>
2018-08-10 15:52:04 -0400

    Refining types for test of abs against legacy pages.
    ARXIVNG-1051

--- tests/legacy_comparison/comparison_types.py
+++ tests/legacy_comparison/comparison_types.py
@@ -3,26 +3,35 @@ from mypy_extensions import TypedDict
 from bs4 import BeautifulSoup
 import requests
 
-res_comparison_fn = Callable[[Any],
-                             TypedDict('response_comparison_args',
-                                       {'ng_url': str,
-                                        'legacy_url': str,
-                                        'ng_res': requests.Response,
-                                        'legacy_res': requests.Response,
-                                        'paperid': str})]
+res_arg_dict = TypedDict('res_arg_dict',
+                         {'ng_url': str,
+                          'legacy_url': str,
+                          'ng_res': requests.Response,
+                          'legacy_res': requests.Response,
+                          'paperid': str})
 
-text_comparison_fn = Callable[[Any],
-                              TypedDict('text_comparison_args',
-                                        {'ng_url': str,
-                                         'legacy_url': str,
-                                         'ng_text': str,
-                                         'legacy_text': str,
-                                         'paperid': str})]
+res_comparison_fn = Callable[[res_arg_dict], Any]
 
-html_comparison_fn = Callable[[Any],
-                              TypedDict('html_comparison_args',
-                                        {'ng_url': str,
-                                         'legacy_url': str,
-                                         'ng_html': BeautifulSoup,
-                                         'legacy_html': BeautifulSoup,
-                                         'paperid': str})]
+text_arg_dict = TypedDict('text_arg_dict',
+                          {'ng_url': str,
+                           'legacy_url': str,
+                           'ng_res': requests.Response,
+                           'legacy_res': requests.Response,
+                           'ng_text': str,
+                           'legacy_text': str,
+                           'paperid': str})
+
+text_comparison_fn = Callable[[text_arg_dict], Any]
+
+html_arg_dict = TypedDict('html_arg_dict',
+                          {'ng_url': str,
+                           'legacy_url': str,
+                           'ng_res': requests.Response,
+                           'legacy_res': requests.Response,
+                           'ng_text': str,
+                           'legacy_text': str,
+                           'ng_html': BeautifulSoup,
+                           'legacy_html': BeautifulSoup,
+                           'paperid': str})
+
+html_comparison_fn = Callable[[html_arg_dict], Any]
---
+++ tests/legacy_comparison/html_comparisons.py
@@ -0,0 +1,10 @@
+
+# html_comparison_fn = Callable[[Any],
+#                               TypedDict('html_comparison_args',
+#                                         {'ng_url': str,
+#                                          'legacy_url': str,
+#                                          'ng_html': BeautifulSoup,
+#                                          'legacy_html': BeautifulSoup,
+#                                          'paperid': str})]
+
+#def compare_abs_fields()
