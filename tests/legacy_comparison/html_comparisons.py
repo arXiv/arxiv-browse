5bf9b80772ddba18b7d405e7517050a7d36c434f
Brian D. Caruso <bdc34@cornell.edu>
2018-08-20 14:56:21 -0400

    Adding more HTML comparisons
    ARXIVNG-1051

--- tests/legacy_comparison/html_comparisons.py
+++ tests/legacy_comparison/html_comparisons.py
@@ -1,25 +1,81 @@
 import logging
+from functools import partial
+from typing import Callable
 
 from tests.legacy_comparison.abstract_comparisons import lev_similarity
 from tests.legacy_comparison.comparison_types import html_arg_dict
 
+from bs4 import BeautifulSoup
+
 
 def html_similarity(html_arg: html_arg_dict) -> str:
     sim = lev_similarity(
-        html_arg['ng_html'].prettify().encode('utf-8').decode('ascii', 'ignore'),
-        html_arg['legacy_html'].prettify().encode('utf-8').decode('ascii', 'ignore')
+        html_arg['ng_html'].prettify().encode(
+            'utf-8').decode('ascii', 'ignore'),
+        html_arg['legacy_html'].prettify().encode(
+            'utf-8').decode('ascii', 'ignore')
     )
     res = f"html_pretty_sim for {html_arg['paperid']} = {sim}"
     if sim < 0.69:
         logging.warning(res)
     return res
 
-# html_comparison_fn = Callable[[Any],
-#                               TypedDict('html_comparison_args',
-#                                         {'ng_url': str,
-#                                          'legacy_url': str,
-#                                          'ng_html': BeautifulSoup,
-#                                          'legacy_html': BeautifulSoup,
-#                                          'paperid': str})]
 
-#def compare_abs_fields()
+def _element_similarity(name: str,
+                        get_element: Callable[[BeautifulSoup], BeautifulSoup],
+                        min_sim: float,
+                        html_arg: html_arg_dict) -> str:
+    """ Uses get_element to select an element of the BS doc on both NG and Legacy do a similarity. """
+    legacy = get_element(html_arg['legacy_html'])
+    ng = get_element(html_arg['ng_html'])
+
+    if len(legacy) != len(ng) or len(ng) != 1 or len(legacy) != 1:
+        res = f"bad counts for {name} for {html_arg['paperid']} ng: {len(ng)} legacy: {len(legacy)}"
+        logging.warning(res)
+        return res
+
+    sim = lev_similarity(
+        ng[0].prettify().encode('utf-8').decode('ascii', 'ignore'),
+        legacy[0].prettify().encode('utf-8').decode('ascii', 'ignore')
+    )
+
+    res = f"similarity for {name} of {html_arg['paperid']} = {sim}"
+    if sim < min_sim:
+        logging.warning(res)
+    return res
+
+
+author_similarity = partial(
+    _element_similarity, 'authors div', lambda bs: bs.select('.authors'), 0.80)
+
+
+dateline_similarity = partial(
+    _element_similarity, 'dateline div', lambda bs: bs.select('.dateline'), 0.8)
+
+
+history_similarity = partial(
+    _element_similarity, 'submission-history div', lambda bs: bs.select('.submission-history'), 0.8)
+
+
+title_similarity = partial(
+    _element_similarity, 'title div', lambda bs: bs.select('.title'), 0.9)
+
+
+endorers_similarity = partial(
+    _element_similarity, 'endorsers div', lambda bs: bs.select('.endorsers'), 0.9)
+
+
+subject_similarity = partial(
+    _element_similarity, 'subjects td', lambda bs: bs.select('.subjects'), 0.98)
+
+
+comments_similarity = partial(
+    _element_similarity, 'comments td', lambda bs: bs.select('.comments'), 0.8)
+
+
+extra_services_similarity = partial(
+    _element_similarity, 'extra-services div', lambda bs: bs.select('.extra-services'), 0.7)
+
+
+head_similarity = partial(
+    _element_similarity, 'head element', lambda bs: bs.select('head'), 0.7)
