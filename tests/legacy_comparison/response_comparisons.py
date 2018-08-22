d5693eb61f35fd0682bce29e399ca32fb8a9a96d
Brian D. Caruso <bdc34@cornell.edu>
2018-08-22 13:19:30 -0400

    Adding more reporting to legacy compare of abs page

--- tests/legacy_comparison/response_comparisons.py
+++ tests/legacy_comparison/response_comparisons.py
@@ -1,15 +1,10 @@
-import logging
-
-from tests.legacy_comparison.comparison_types import res_arg_dict
+from tests.legacy_comparison.comparison_types import res_arg_dict, BadResult
 
 
-def compare_status(res_arg: res_arg_dict) -> str:
+def compare_status(res_arg: res_arg_dict) -> BadResult:
     if res_arg['ng_res'].status_code == 200 and res_arg['legacy_res'].status_code == 200:
-        res = f'200 HTTP status for both {res_arg["ng_url"]} and {res_arg["legacy_url"]}'
-        # logging.info(res)
-        return res
+        return None
     else:
         res = f'HTTP status for {res_arg["ng_url"]} was {res_arg["ng_res"].status_code} ' \
               f'and for {res_arg["legacy_url"]} was {res_arg["legacy_res"].status_code}'
-        logging.warning(res)
-        return res
+        return BadResult(res_arg['paper_id'], 'compare_status', res)
