1c6dfbea4504294bf25d5814ee3365b6a32d5cd0
Brian D. Caruso <bdc34@cornell.edu>
2018-09-11 16:09:51 -0400

    Working on test against legacy, adding required metadata fields
    ARXIVNG-1051

--- tests/legacy_comparison/html_comparisons.py
+++ tests/legacy_comparison/html_comparisons.py
@@ -19,16 +19,44 @@ def html_similarity(html_arg: html_arg_dict) -> BadResult:
     return None
 
 
+def metadata_fields_similarity( html_arg: html_arg_dict) -> BadResult:
+    # There should be a div.metatable
+    # It may have others and if it does they must be on both NG an legacy pages.
+
+    ng_trs = html_arg['ng_html'].find('div', 'metatable').find_all('tr')
+    legacy_trs = html_arg['legacy_html'].find('div', 'metatable').find_all('tr')
+
+    to_label = lambda tr: tr.find('td', 'label').contents
+    ng_labels=set(ng_trs.map(to_label))
+    legacy_labels = set(legacy_trs.map(to_label))
+
+    if ng_labels == legacy_labels:
+        return None
+    else:
+        return BadResult(html_arg['paper_id'],
+                         "Metadata field included on NG do not match those from legacy" +
+                         f"NG: {ng_labels} Legacy: {legacy_labels}")
+
+
 def _element_similarity(name: str,
                         get_element: Callable[[BeautifulSoup], BeautifulSoup],
                         min_sim: float,
+                        required: bool,
                         html_arg: html_arg_dict) -> BadResult:
     """ Uses get_element to select an element of the BS doc on both NG and Legacy do a similarity. """
     legacy = get_element(html_arg['legacy_html'])
     ng = get_element(html_arg['ng_html'])
 
-    if len(ng) == 0 and len(legacy) == 0:
-        return None
+    if required:
+        if len(ng) == 0 and len(legacy) == 0:
+            return BadResult(html_arg['paper_id'], name,
+                             f"Missing field {name} for {html_arg['paper_id']} from NG and Legacy")
+        if len(ng) == 0:
+            return BadResult(html_arg['paper_id'], name,
+                             f"Missing field {name} for {html_arg['paper_id']} from NG")
+        if len(legacy) == 0:
+            return BadResult(html_arg['paper_id'], name,
+                             f"Missing field {name} for {html_arg['paper_id']} from legacy")
 
     if len(legacy) != len(ng) or len(ng) != 1 or len(legacy) != 1:
         return BadResult(html_arg['paper_id'], name,
@@ -46,32 +74,33 @@ def _element_similarity(name: str,
 
 
 author_similarity = partial(
-    _element_similarity, 'authors div', lambda bs: bs.select('.authors'), 0.80)
+    _element_similarity, 'authors div', lambda bs: bs.select('.authors'), 0.9, True)
 
 
 dateline_similarity = partial(
-    _element_similarity, 'dateline div', lambda bs: bs.select('.dateline'), 0.8)
+    _element_similarity, 'dateline div', lambda bs: bs.select('.dateline'), 0.8, True)
 
 
 history_similarity = partial(
-    _element_similarity, 'submission-history div', lambda bs: bs.select('.submission-history'), 0.8)
+    _element_similarity, 'submission-history div', lambda bs: bs.select('.submission-history'), 0.9, True)
 
 
 title_similarity = partial(
-    _element_similarity, 'title div', lambda bs: bs.select('.title'), 0.9)
+    _element_similarity, 'title div', lambda bs: bs.select('.title'), 0.9, True)
 
 
 subject_similarity = partial(
-    _element_similarity, 'subjects td', lambda bs: bs.select('.subjects'), 0.98)
+    _element_similarity, 'subjects td', lambda bs: bs.select('.subjects'), 0.98, True)
 
 
 comments_similarity = partial(
-    _element_similarity, 'comments td', lambda bs: bs.select('.comments'), 0.8)
+    _element_similarity, 'comments td', lambda bs: bs.select('.comments'), 0.9, False)
 
 
 extra_services_similarity = partial(
-    _element_similarity, 'extra-services div', lambda bs: bs.select('.extra-services'), 0.7)
+    _element_similarity, 'extra-services div', lambda bs: bs.select('.extra-services'), 0.9, False)
 
 
 head_similarity = partial(
-    _element_similarity, 'head element', lambda bs: bs.select('head'), 0.7)
+    _element_similarity, 'head element', lambda bs: bs.select('head'), 0.85, True)
+
