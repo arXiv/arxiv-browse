30cb9e7d70a9feac360a56a1116b34728d21c880
Martin Lessmeister <mhl10@cornell.edu>
2018-09-12 17:00:49 -0400

    timezone fix

--- browse/services/util/response_header.py
+++ browse/services/util/response_header.py
@@ -2,7 +2,8 @@
 from datetime import datetime, timedelta, timezone
 from dateutil.tz import tzutc, gettz
 from typing import Tuple
-from browse.services.document.metadata import ARXIV_BUSINESS_TZ
+
+from arxiv.base.globals import get_application_config
 
 
 def guess_next_update(dt: datetime = datetime.utcnow()) \
@@ -13,31 +14,31 @@ def guess_next_update(dt: datetime = datetime.utcnow()) \
     Guess is based on provided datetime.
     """
     # local_dt = datetime.now(tz=ARXIV_BUSINESS_TZ)
+    config = get_application_config()
+    tz = gettz(config.get('ARXIV_BUSINESS_TZ', 'US/Eastern'))
     publish_dt = datetime(year=dt.year, month=dt.month,
                           day=dt.day, hour=20, minute=0, second=0,
-                          microsecond=0, tzinfo=gettz(ARXIV_BUSINESS_TZ))
-                          # microsecond=0, tzinfo=tzutc())
-    # approx_publish_duration = timedelta(hours=1, minutes=30)
+                          microsecond=0, tzinfo=tz)
+
+    approx_publish_duration = timedelta(hours=1, minutes=30)
     before_todays_publish: bool = dt < publish_dt
     if before_todays_publish:
         # It's before today's publish time
         delta_to_next_publish = publish_dt - dt
     else:
         # It's after today's publish time
-        print(f"after today's publish time, weekday={dt.isoweekday()}")
         delta_to_next_publish = publish_dt + timedelta(days=1) - dt
 
     if dt.isoweekday() == 5 and not before_todays_publish:
         # It's Friday and after publish (or roughly after freeze)
-        print("got here")
         delta_to_next_publish = delta_to_next_publish + timedelta(days=1)
     # elif dt.isoweekday() == 6:
     #     # It's Saturday
     #     delta_to_next_publish = delta_to_next_publish + timedelta(days=1)
 
     next_publish_dt = dt + delta_to_next_publish
-    print(f"\ndt: {dt}\nnext: {next_publish_dt}")
-    return next_publish_dt.astimezone(tz=timezone.utc)
+    # print(f"\ndt: {dt}\nnext: {next_publish_dt}")
+    return next_publish_dt.astimezone(tz=tzutc())
 
 def mime_header_date(dt: datetime) -> str:
     """Convert a datetime to string in MIME date format (RFC 1123)."""
--- tests/test_response_headers.py
+++ tests/test_response_headers.py
@@ -1,26 +1,44 @@
 """Test utility functions for generating response headers."""
 from unittest import TestCase
 from datetime import datetime
-from dateutil.tz import tzutc
-
+from dateutil.tz import tzutc, gettz
 
 from browse.services.util.response_header import guess_next_update, mime_header_date
-from browse.services.document.metadata import ARXIV_BUSINESS_TZ
-
+from arxiv.base.globals import get_application_config
 
 class TestResponseHeaderUtils(TestCase):
     """Test response header utility functions."""
 
     def test_guess_next_update(self) -> None:
         """Test the guess_next_update function."""
-        dt = datetime(year=2018, month=9, day=11, hour=19, minute=0, tzinfo=tzutc())
+
+        config = get_application_config()
+        tz = gettz('US/Eastern')
+
+
+        dt = datetime(year=2018, month=9, day=11, hour=19, minute=59, tzinfo=tz)
         next_publish_dt = guess_next_update(dt)
-        expected_publish_dt = datetime(year=2018, month=9, day=11, hour=20, minute=0, tzinfo=tzutc())
+        expected_publish_dt = datetime(year=2018, month=9, day=12, hour=0, minute=0, tzinfo=tzutc())
         self.assertEquals(next_publish_dt, expected_publish_dt)
 
-        dt = datetime(year=2018, month=9, day=14, hour=20, minute=1, tzinfo=tzutc())
+        dt = datetime(year=2018, month=9, day=11, hour=20, minute=1, tzinfo=tzutc())
         next_publish_dt = guess_next_update(dt)
-        expected_publish_dt = datetime(year=2018, month=9, day=16, hour=20, minute=0, tzinfo=tzutc())
+        expected_publish_dt = datetime(year=2018, month=9, day=12, hour=0, minute=0, tzinfo=tzutc())
+        self.assertEquals(next_publish_dt, expected_publish_dt)
+
+        dt = datetime(year=2018, month=9, day=11, hour=20, minute=0, tzinfo=tz)
+        next_publish_dt = guess_next_update(dt)
+        expected_publish_dt = datetime(year=2018, month=9, day=13, hour=0, minute=0, tzinfo=tzutc())
+        self.assertEquals(next_publish_dt, expected_publish_dt)
+
+        dt = datetime(year=2018, month=9, day=11, hour=20, minute=1, tzinfo=tz)
+        next_publish_dt = guess_next_update(dt)
+        expected_publish_dt = datetime(year=2018, month=9, day=13, hour=0, minute=0, tzinfo=tzutc())
+        self.assertEquals(next_publish_dt, expected_publish_dt)
+
+        dt = datetime(year=2018, month=9, day=14, hour=20, minute=1, tzinfo=tz)
+        next_publish_dt = guess_next_update(dt)
+        expected_publish_dt = datetime(year=2018, month=9, day=17, hour=00, minute=0, tzinfo=tzutc())
         self.assertEquals(next_publish_dt, expected_publish_dt)
 
     def test_mime_header_date(self) -> None:
