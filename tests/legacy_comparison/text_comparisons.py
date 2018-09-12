76ee5ec82655c8b9a3491454e7ebeb5096e5fbeb
Brian D. Caruso <bdc34@cornell.edu>
2018-09-12 15:13:43 -0400

    Fixing bug with legacy comparison for text, and removal of hrefs

--- tests/legacy_comparison/text_comparisons.py
+++ tests/legacy_comparison/text_comparisons.py
@@ -3,8 +3,9 @@ from tests.legacy_comparison.comparison_types import text_arg_dict, BadResult
 
 
 def text_similarity(text_arg: text_arg_dict) -> BadResult:
-    if lev_similarity(text_arg['ng_text'], text_arg['legacy_text']) < 0.74:
-        res = f"text_sim for {text_arg['paperid']}  = {sim}"
+    sim = lev_similarity(text_arg['ng_text'], text_arg['legacy_text'])
+    if sim < 0.74:
+        res = f"text_sim for {text_arg['paper_id']}  = {sim}"
         return BadResult(text_arg['paper_id'], 'text_similarity', res)
     else:
         return None
