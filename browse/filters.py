072b6c3e7d507f76b3ff18d037881be32df94808
Brian D. Caruso <bdc34@cornell.edu>
2018-10-02 12:56:41 -0400

    Adding checks for lt and gt in entity_to_utf filter

--- browse/filters.py
+++ browse/filters.py
@@ -191,6 +191,35 @@ def arxiv_id_urls(text: JinjaFilterInput) -> Markup:
     return Markup(result)
 
 
+def entity_to_utf(text: str) -> str:
+    """Converts HTML entities to unicode.  
+
+    For example '&amp;' becomes '&'.
+
+    Must be first filter in list because it does not do anything to a
+    Markup. On a Markup object it will do nothing and just return the
+    input Markup.
+
+    DANGEROUS because this is basically an unescape.
+    It tries to avoid junk like <script> but it is a bad idea.
+    This MUST NEVER BE USED ON USER PROVIDED INPUT. Submission titles etc.
+
+    """
+
+    if hasattr(text, '__html__'):
+        return text
+    
+    without_lt = re.sub('<', 'XXX_LESS_THAN_XXX', text)
+    without_lt_gt = re.sub('>', 'XXX_GREATER_THAN_XXX', without_lt)
+
+    unes = html.unescape(without_lt_gt)
+
+    with_lt = re.sub('XXX_LESS_THAN_XXX', '&lt;', unes)
+    with_lt_gt = re.sub('XXX_GREATER_THAN_XXX', '&gt;', with_lt)
+
+    return Markup(with_lt_gt)
+
+
 def tex_to_utf(text: JinjaFilterInput) -> Markup:
     """Wraps tex2utf as a filter."""
 
@@ -198,5 +227,4 @@ def tex_to_utf(text: JinjaFilterInput) -> Markup:
         etxt = text
     else:
         etxt = Markup(escape(text))
-
     return Markup(tex2utf(etxt))
