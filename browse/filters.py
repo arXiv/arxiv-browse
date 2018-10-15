845a55f3ed836526b28aee4bac200acc0539ae29
Martin Lessmeister <mhl10@cornell.edu>
2018-10-15 14:42:21 -0400

    minor

--- browse/filters.py
+++ browse/filters.py
@@ -1,28 +1,28 @@
 """Browse jinja filters."""
 import re
 from urllib import parse
-from typing import Callable, Match, Optional, Union
+from typing import Callable, Union
 
 from jinja2 import Markup, escape
-from jinja2._compat import text_type
-from jinja2.utils import _digits, _letters  # type: ignore
-from flask import url_for
 import html
 
 from browse.services.util.tex2utf import tex2utf
 
 JinjaFilterInput = Union[Markup, str]
-"""Jinja filters will receive their text input as either
+"""
+   Jinja filters will receive their text input as either
    a Markup object or a str. It is critical for proper escaping to
    to ensure that str is correctly HTML escaped.
 
-   Markup is decnded from str so this type is redundent but
+   Markup is decoded from str so this type is redundant but
    the hope is to make it clear what is going on to arXiv developers.
 """
 
 
-def single_doi_url(clickthrough_url_for: Callable[[str], str], doi: JinjaFilterInput) -> Markup:
-    """DOI is made into a link.
+def single_doi_url(clickthrough_url_for: Callable[[str], str],
+                   doi: JinjaFilterInput) -> Markup:
+    """
+    DOI is made into a link.
 
     This expects a DOI ONLY. It should not be used on general text.
 
@@ -32,15 +32,13 @@ def single_doi_url(clickthrough_url_for: Callable[[str], str], doi: JinjaFilterI
     How does this ensure escaping? It expects just a DOI, The result
     is created as a properly escaped Markup.
     """
-
     doi_url = f'https://dx.doi.org/{parse.quote_plus(doi)}'
     ct_url = clickthrough_url_for(doi_url)
     return Markup(f'<a href="{ct_url}">{escape(doi)}</a>')
 
 
 def line_feed_to_br(text: JinjaFilterInput) -> Markup:
-    """Lines that start with two spaces should be broken"""
-
+    """Lines that start with two spaces should be broken."""
     if hasattr(text, '__html__'):
         etxt = text
     else:
@@ -53,7 +51,8 @@ def line_feed_to_br(text: JinjaFilterInput) -> Markup:
 
 
 def entity_to_utf(text: str) -> str:
-    """Converts HTML entities to unicode.
+    """
+    Convert HTML entities to unicode.
 
     For example '&amp;' becomes '&'.
 
@@ -65,7 +64,6 @@ def entity_to_utf(text: str) -> str:
     It tries to avoid junk like <script> but it is a bad idea.
     This MUST NEVER BE USED ON USER PROVIDED INPUT. Submission titles etc.
     """
-
     # TODO it would be good to move this out of a jinja filter
     # and to the controller, it is only used for things coming from DBLP
     if hasattr(text, '__html__'):
@@ -83,8 +81,7 @@ def entity_to_utf(text: str) -> str:
 
 
 def tex_to_utf(text: JinjaFilterInput) -> Markup:
-    """Wraps tex2utf as a filter."""
-
+    """Wrap tex2utf as a filter."""
     if hasattr(text, '__html__'):
         # Need to unescape so nothing that is tex is escaped
         return Markup(escape(tex2utf(text.unescape())))  # type: ignore
