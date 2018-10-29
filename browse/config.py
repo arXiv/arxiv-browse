358a9d54387c35d5b814c26e603a8af6dad421ac
Martin Lessmeister <mhl10@cornell.edu>
2018-10-29 10:16:20 -0400

    Add BROWSE_SQLALCHEMY_POOL_SIZE and BROWSE_SQLALCHEMY_MAX_OVERFLOW config options.

--- browse/config.py
+++ browse/config.py
@@ -209,6 +209,13 @@ SQLALCHEMY_DATABASE_URI = os.environ.get(
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_RECORD_QUERIES = False
+# SQLALCHEMY_POOL_SIZE and SQLALCHEMY_MAX_OVERFLOW are set without defaults
+# because they will not work with sqlite
+SQLALCHEMY_POOL_SIZE = os.environ.get(
+    'BROWSE_SQLALCHEMY_POOL_SIZE', None)
+SQLALCHEMY_MAX_OVERFLOW = os.environ.get(
+    'BROWSE_SQLALCHEMY_MAX_OVERFLOW', None)
+
 
 # Disable DB queries even if other SQLAlchemy config are defined
 # This, for example, could be used in conjunction with the `no-write` runlevel
