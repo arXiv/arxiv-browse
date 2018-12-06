0a8ae9db3f916072823f22ace1e62f32c092c000
Brian D. Caruso <bdc34@cornell.edu>
2018-12-06 14:04:59 -0500

    Uses arxiv-base abs_macros. Loading templates from multiple sources
    Loading templates for abs page from both arxiv.browse.templates and
    browse.templates.

--- browse/factory.py
+++ browse/factory.py
@@ -2,25 +2,32 @@
 from functools import partial
 from typing import Any, Optional, Callable, Dict
 from flask import Flask, url_for
-from arxiv.browse.domain.identifier import canonical_url
+import jinja2
+
 from browse.util.clickthrough import create_ct_url
-from arxiv.browse.util.id_patterns import do_dois_id_urls_to_tags, do_id_to_tags, \
-    do_dois_arxiv_ids_to_tags
 from browse.routes import ui
 from browse.services.database import models
 from browse.services.util.email import generate_show_email_hash
-from arxiv.browse.filters import line_feed_to_br, tex_to_utf, entity_to_utf, \
-    single_doi_url
 
 from arxiv.base.config import BASE_SERVER
 from arxiv.base import Base
-
+from arxiv.browse.setup_jinja_for_abs import setup_jinja_for_abs
+from arxiv.browse.template_loader import browse_template_loader
 
 def create_web_app() -> Flask:
     """Initialize an instance of the browse web application."""
     app = Flask('browse', static_folder='static', template_folder='templates')
     app.config.from_pyfile('config.py')
 
+    # load templates from multiple sources
+    # first on list gets perference so browse could override arxiv.browse
+    multi_loader = jinja2.ChoiceLoader([
+        app.jinja_loader,
+        browse_template_loader()
+    ])
+
+    app.jinja_loader = multi_loader
+    
     # TODO Only needed until this route is added to arxiv-base
     if 'URLS' not in app.config:
         app.config['URLS'] = []
@@ -45,43 +52,3 @@ def create_web_app() -> Flask:
     
     return app
 
-
-def setup_jinja_for_abs(jinja_env: Any,
-                        ct_url_for: Callable[[str],str],
-                        id_to_url: Callable[[str],Any],
-                        email_hash: Callable[[str],Optional[str]])->None:
-    """Add filters and functions to jinja_env to suppor the abs page macros"""
-    
-    if not jinja_env.globals:
-        jinja_env.globals = {}
-
-    jinja_env.globals['canonical_url'] = canonical_url
-
-    def ct_single_doi_filter(doi: str)->str:
-        return single_doi_url(ct_url_for, doi)
-
-    def contextualized_id_filter(text: str)->str:
-        return do_id_to_tags(id_to_url, text)
-
-    def contextualized_doi_id_url_filter(text: str)->str:
-        return do_dois_id_urls_to_tags(id_to_url, ct_url_for, text)
-
-    def ct_doi_filter(text: str)->str:
-        return do_dois_arxiv_ids_to_tags(id_to_url,
-                                         ct_url_for,
-                                         text)
-
-    if not jinja_env.filters:
-        jinja_env.filters = {}
-
-    jinja_env.filters['line_feed_to_br'] = line_feed_to_br
-    jinja_env.filters['tex_to_utf'] = tex_to_utf
-    jinja_env.filters['entity_to_utf'] = entity_to_utf
-
-    jinja_env.filters['clickthrough_url_for'] = ct_url_for
-    jinja_env.filters['show_email_hash'] = email_hash
-    
-    jinja_env.filters['single_doi_url'] = ct_single_doi_filter
-    jinja_env.filters['arxiv_id_urls'] = contextualized_id_filter
-    jinja_env.filters['arxiv_urlize'] = contextualized_doi_id_url_filter
-    jinja_env.filters['arxiv_id_doi_filter'] = ct_doi_filter
