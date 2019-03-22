c054139653f15556a666886768a081db2635bbdd
Brian D. Caruso <bdc34@cornell.edu>
2019-03-22 09:46:21 -0400

    Going back to skipping mypy tests for travis

--- tests/test_typings.py
+++ tests/test_typings.py
@@ -11,15 +11,15 @@ from unittest import TestCase
 class MyPyTest(TestCase):
     """Class for testing modules with mypy."""
 
-
+    @unittest.skip("Type checkes skipped due to failing with out of memory on travis")
     def test_run_mypy_module(self) -> None:
         """Run mypy on all module sources."""
         mypy_call: List[str] = ["mypy"] + self.mypy_opts + ["-p", self.pkgname]
         result: int = subprocess.call(
             mypy_call, env=os.environ, cwd=self.pypath)
         self.assertEqual(result, 0, f'mypy on {self.pkgname}')
-        
 
+    @unittest.skip("Type checkes skipped due to failing with out of memory on travis")
     def test_run_mypy_tests(self) -> None:
         """Run mypy on all tests in module under the tests directory."""
 
