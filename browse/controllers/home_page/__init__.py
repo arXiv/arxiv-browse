8108b2516fc34bb99e4556c0075d83d708dd65eb
Martin Lessmeister <mhl10@cornell.edu>
2019-04-16 09:41:13 -0400

    cleanup

--- browse/controllers/home_page/__init__.py
+++ browse/controllers/home_page/__init__.py
@@ -24,18 +24,17 @@ def get_home_page() -> Response:
     """Get the data needed to generated the home page."""
     response_data: Dict[str, Any] = {}
     response_headers: Dict[str, Any] = {}
-
     try:
-        response_data['groups'] = taxonomy.definitions.GROUPS
-        response_data['archives'] = taxonomy.definitions.ARCHIVES_ACTIVE
-        response_data['categories'] = taxonomy.definitions.CATEGORIES_ACTIVE
         response_data['document_count'] = _get_document_count()
-        response_status = status.HTTP_200_OK
     except Exception as ex:
         logger.warning(f'Could not get abs page data: {ex}')
         raise InternalServerError
 
-    return response_data, response_status, response_headers
+    response_data['groups'] = taxonomy.definitions.GROUPS
+    response_data['archives'] = taxonomy.definitions.ARCHIVES_ACTIVE
+    response_data['categories'] = taxonomy.definitions.CATEGORIES_ACTIVE
+
+    return response_data, status.HTTP_200_OK, response_headers
 
 
 def _get_document_count() -> Optional[int]:
