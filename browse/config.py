fbf4bc4d17c6d1f05aef2d386e1eb8fd720b5156
Martin  Lessmeister <mhl10@lib-arxiv-016.serverfarm.cornell.edu>
2019-11-04 10:47:59 -0500

    Fix FLASKS3 boolean config options

--- browse/config.py
+++ browse/config.py
@@ -210,9 +210,9 @@ See `<https://flask-s3.readthedocs.io/en/latest/>`_.
 """
 FLASKS3_BUCKET_NAME = os.environ.get('FLASKS3_BUCKET_NAME', 'some_bucket')
 FLASKS3_CDN_DOMAIN = os.environ.get('FLASKS3_CDN_DOMAIN', 'static.arxiv.org')
-FLASKS3_USE_HTTPS = os.environ.get('FLASKS3_USE_HTTPS', 1)
-FLASKS3_FORCE_MIMETYPE = os.environ.get('FLASKS3_FORCE_MIMETYPE', 1)
-FLASKS3_ACTIVE = os.environ.get('FLASKS3_ACTIVE', 0)
+FLASKS3_USE_HTTPS = bool(int(os.environ.get('FLASKS3_USE_HTTPS', 1)))
+FLASKS3_FORCE_MIMETYPE = bool(int(os.environ.get('FLASKS3_FORCE_MIMETYPE', '1')))
+FLASKS3_ACTIVE = bool(int(os.environ.get('FLASKS3_ACTIVE', '0')))
 
 # SQLAlchemy configuration
 # For mysql: 'mysql://user:pass@localhost/dbname'
