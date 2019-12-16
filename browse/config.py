93ec6511b450931dc4c5f7f308839eb8ae710219
Martin Lessmeister <mhl10@cornell.edu>
2019-12-15 21:24:48 -0500

    bump app version

--- browse/config.py
+++ browse/config.py
@@ -8,7 +8,7 @@ import warnings
 import dateutil.parser
 from datetime import datetime, timedelta
 
-APP_VERSION = '0.2.6'
+APP_VERSION = '0.2.7'
 """The application version """
 
 ON = 'yes'
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ author = 'arXiv-NG Team'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.2.7'
 
 
 # -- General configuration ---------------------------------------------------
