d0b2acba2545e670a828c6444ec8f0da4d52e325
Martin Lessmeister <mhl10@cornell.edu>
2020-07-09 21:44:16 -0400

    move endorsers section

--- browse/static/js/toggle-labs.js
+++ browse/static/js/toggle-labs.js
@@ -1,8 +1,9 @@
 // vanilla toggle script for LABS enabling
 $(document).ready(function() {
 
-  jQuery.cachedScript = function(url, options) {
+  var bibex_script = "https://static.arxiv.org/js/bibex-dev/bibex.js";
 
+  jQuery.cachedScript = function(url, options) {
     // Allow user to set any option except for dataType, cache, and url
     options = $.extend(options || {}, {
       dataType: "script",
@@ -17,10 +18,20 @@ $(document).ready(function() {
 
   var labsCookie = Cookies.getJSON("arxiv_labs");
   if (labsCookie) {
+    has_enabled = false;
     for (var key in labsCookie) {
       if (labsCookie[key] && labsCookie[key] == "enabled") {
+        has_enabled = true;
         $("#" + key + ".lab-toggle").toggleClass("enabled", true);
+        if (key == "bibex-toggle") {
+          $.cachedScript(bibex_script).done(function(script, textStatus) {
+            console.log(textStatus);
+          });
+        }
       }
+    }
+    if ( has_enabled ) {
+      $('.labs-display').show();
     }
   } else {
     Cookies.set("arxiv_labs", {});
@@ -35,6 +46,7 @@ $(document).ready(function() {
     var bibex_val = false;
     $(this).toggleClass("enabled");
     if ($(this).hasClass("enabled")) {
+      $('.labs-display').show();
       cookie_val = "enabled";
       bibex_val = true;
     }
@@ -42,7 +54,7 @@ $(document).ready(function() {
       bibexCookie[bibex_key] = bibex_val;
       Cookies.set("arxiv_bibex", bibexCookie);
       if (bibex_val) {
-        $.cachedScript("/static/browse/0.3.0/bibex/bibex.js").done(function(script, textStatus) {
+        $.cachedScript(bibex_script).done(function(script, textStatus) {
           console.log(textStatus);
         });
       }
