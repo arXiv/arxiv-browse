ef39b1209e1248a6dcf8fd2396b14083792e2b0b
Brian D. Caruso <bdc34@cornell.edu>
2020-09-22 13:14:12 -0400

    Adding loading msg to bibtex modal.
    ARXIVNG-3519

--- browse/static/js/cite.js
+++ browse/static/js/cite.js
@@ -1,29 +1,62 @@
 // bibtex citation modal 
-
 $(document).ready(function() {
-    $('.bib-modal-close').click(function() {
-        $('#bib-cite-modal').hide();
-    });
+    var cached_value=null
+    var cached_provider=null
+    var setup=0
+    var API_CROSSREF_CITE='https://dx.doi.org/'
 
+    var provider_desc={
+        arxiv: 'arXiv API',
+        doi: 'Crossref Citation Formating Service'
+    }
 
-    var cssloaded=0;
+    var provider_url={
+        arxiv: 'https://arxiv.org/help/api/index',
+        doi: 'https://www.crossref.org/labs/citation-formatting-service/'
+    }
+
+    var format_crossref=function(res){
+        //TODO
+        return res
+    }
+    
     $('#bib-cite-trigger').click(function(){
-        meta =  document.head.querySelector(`[name="citation_arxiv_id"]`);
-        id = meta ? meta.content: '';
+        $('#bib-cite-loading').show()
+        
+        metaid =  document.head.querySelector(`[name="citation_arxiv_id"]`)
+        id = metaid ? metaid.content: ''
 
-        if( ! cssloaded ){
-            $('<link>').appendTo('head')  //load css for cite modal
-                .attr({type:'text/css',
-                       rel: 'stylesheet',
-                       href: $('#bib-cite-css').attr('href')});
-            cssloaded = 1;
+        metadoi =  document.head.querySelector(`[name="citation_doi"]`)
+        doi = metadoi ? metadoi.content: ''
+
+        if(!setup){
+            $('.bib-modal-close').click(function(){$('#bib-cite-modal').hide()})
+            $('<link>').appendTo('head').attr({type:'text/css',rel: 'stylesheet',href: $('#bib-cite-css').attr('href')})
+            setup = 1
         }
 
-        $.ajax({url: "/bibtex/" + id,
-                success:function(result){
-                    $('#bib-cite-target').val(result);
-                    $('#bib-cite-modal').show();
-                }
-               } );
-    } );
-});
+        var do_modal=function(result, provider){
+            $('#bib-cite-loading').hide()
+            $('#bib-cite-target').val(result)
+            $('#bib-cite-source-api').text(provider_desc[provider]).attr('href', provider_url[provider])
+            $('#bib-cite-modal').show()
+        }
+
+        if(cached_value){
+            do_modal(cached_value, cached_provider)
+        }else if(doi){
+            $.ajax({url: API_CROSSREF_CITE + doi,
+                    headers: {Accept: `text/bibliography; style=bibtex`},
+                    success: function(result){
+                        cached_value = format_crossref(result)
+                        cached_provider = 'doi'
+                        do_modal( cached_value, cached_provider )}})
+        }else{
+            $.ajax({url: "/bibtex/" + id,
+                    success: function(result){
+                        cached_value = result
+                        cached_provider = 'arxiv'
+                        do_modal( cached_value, cached_provider)}})
+        }
+    })
+})
