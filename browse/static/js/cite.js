179712079458f7ddbc65fa008535dbbeff1053d4
Brian D. Caruso <bdc34@cornell.edu>
2020-09-22 10:39:44 -0400

    Getting paper ID for bibtex from meta.
    ARXIVNG-3519

--- browse/static/js/cite.js
+++ browse/static/js/cite.js
@@ -1,23 +1,29 @@
 // bibtex citation modal 
 
 $(document).ready(function() {
-    $('.bib-modal-close').click(function(){
-        console.log('clicking to close');
+    $('.bib-modal-close').click(function() {
         $('#bib-cite-modal').hide();
     });
 
-    $('#bib-cite-trigger').click(function(){
-        $('<link>').appendTo('head')  //load css for cite modal
-             .attr({type:'text/css',
-                 rel: 'stylesheet',
-                 href: $('#bib-cite-css').attr('href')});
 
-        $.ajax({url: "/bibtex/0704.0526", 
+    var cssloaded=0;
+    $('#bib-cite-trigger').click(function(){
+        meta =  document.head.querySelector(`[name="citation_arxiv_id"]`);
+        id = meta ? meta.content: '';
+
+        if( ! cssloaded ){
+            $('<link>').appendTo('head')  //load css for cite modal
+                .attr({type:'text/css',
+                       rel: 'stylesheet',
+                       href: $('#bib-cite-css').attr('href')});
+            cssloaded = 1;
+        }
+
+        $.ajax({url: "/bibtex/" + id,
                 success:function(result){
-                    console.log('success with result: ' + result);
                     $('#bib-cite-target').val(result);
                     $('#bib-cite-modal').show();
                 }
-               });
+               } );
     } );
 });
