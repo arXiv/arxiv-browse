33f0797eacbaba38174f0bcccc467fe319d7d067
ludoviofb <ludovio@fb.com>
2020-10-14 16:28:03 +0200

    Inline css and remove debug statement

--- browse/static/js/paperwithcode.js
+++ browse/static/js/paperwithcode.js
@@ -1,13 +1,5 @@
 (function () {
-  var styleUrl = $('#paperwithcode-toggle').data('style-url');
-  var linkElement = document.createElement('link');
-  linkElement.setAttribute('rel', 'stylesheet');
-  linkElement.setAttribute('type', 'text/css');
-  linkElement.setAttribute('href', styleUrl);
-  document.head.appendChild(linkElement);
-
   var arxivId = window.location.pathname.split('/').reverse()[0];
-  // arxivId = '1909.11065';
   var pwcApiUrl = 'https://arxiv.paperswithcode.com/api/v0/papers/' + arxivId;
   var $output = $('#pwc-output');
   $.get(pwcApiUrl).done(function (response) {
