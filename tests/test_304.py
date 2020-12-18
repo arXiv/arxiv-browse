d8c5c4b9a88d73c21aa9b8ba6b8f934adec99d24
Brian D. Caruso <bdc34@cornell.edu>
2020-12-18 16:48:23 -0500

    Tests for modified abs
    ARXIVNG-3783

--- tests/test_304.py
+++ tests/test_304.py
@@ -1,4 +1,8 @@
 import unittest
+from dateutil import parser
+from datetime import timedelta
+
+from browse.services.util.response_headers import mime_header_date
 
 from app import app
 
@@ -9,13 +13,14 @@ class ListPageTest(unittest.TestCase):
         self.app = app
         self.client = app.test_client()
 
-    def test_not_modified(self):
+    def test_modified(self):
         with self.app.app_context():
             rv = self.client.get('/abs/0704.0600')
             self.assertEqual(rv.status_code, 200)
 
             last_mod = rv.headers['Last-Modified']
             etag = rv.headers['ETag']
+            
             rv = self.client.get('/abs/0704.0600',
                                  headers={'If-Modified-Since': last_mod})
             self.assertEqual(rv.status_code, 304)
@@ -47,4 +52,23 @@ class ListPageTest(unittest.TestCase):
             rv = self.client.get('/abs/0704.0600',
                                  headers={'iF-NoNE-MaTCH': etag})
             self.assertEqual(rv.status_code, 304)
-            
+
+    def test_not_modified(self):
+        with self.app.app_context():
+            rv = self.client.get('/abs/0704.0600')
+            self.assertEqual(rv.status_code, 200)
+
+            mod_dt = parser.parse(rv.headers['Last-Modified'])
+
+            rv = self.client.get('/abs/0704.0600',
+                                 headers={'If-Modified-Since': mime_header_date(mod_dt )})
+            self.assertEqual(rv.status_code, 304)
+
+            rv = self.client.get('/abs/0704.0600',
+                                 headers={'If-Modified-Since': mime_header_date(mod_dt + timedelta(seconds=-1))})
+            self.assertEqual(rv.status_code, 200)
+
+
+            rv = self.client.get('/abs/0704.0600',
+                                 headers={'If-None-Match': '"should-never-match"'})
+            self.assertEqual(rv.status_code, 200)
