58840b11bacc6d608c66b31df24db3eacd8bf95b
Itay KanHar <knaan.harpaz@gmail.com>
2021-01-22 03:09:56 +0200

    Renamed variables

--- browse/static/js/connectedpapers.js
+++ browse/static/js/connectedpapers.js
@@ -8,13 +8,13 @@
     return;
   }
 
-  const script_path = document.getElementById('connectedpapers-toggle').attributes["data-script-url"].value;
-  const script_dir = script_path.substr(0, script_path.lastIndexOf('/'));
+  const scriptPath = document.getElementById('connectedpapers-toggle').attributes["data-script-url"].value;
+  const scriptDir = scriptPath.substr(0, scriptPath.lastIndexOf('/'));
 
-  const css_loader = '<link rel="stylesheet" type="text/css" href="' + script_dir + '/connectedpapers.css"/>';
-  const loading_html = '<p>Loading...</p>';
+  const cssLoader = '<link rel="stylesheet" type="text/css" href="' + scriptDir + '/connectedpapers.css"/>';
+  const loadingHtml = '<p>Loading...</p>';
   
-  $output.html(css_loader + loading_html);
+  $output.html(cssLoader + loadingHtml);
 
 
   const REST_ADDR = 'https://rest.migration.connectedpapers.com/';
@@ -35,14 +35,14 @@
       return;
     }
     if (translationResponse == null) {
-      $output.html(css_loader + idNotRecognizedHtml);
+      $output.html(cssLoader + idNotRecognizedHtml);
       return;
     }
     const paperId = translationResponse.paperId;
     const title = translationResponse.title;
 
     if (paperId.length == 0 || title.length == 0) {
-      $output.html(css_loader + idNotRecognizedHtml);
+      $output.html(cssLoader + idNotRecognizedHtml);
       return;
     }
 
@@ -56,10 +56,9 @@
 
       const graphUrl = CONNECTED_PAPERS_ADDR + 'main/' + paperId + '/arxiv';
       
-      const buildGraphLinkHtml = '<p class="connectedpapers-title">' + title '</p>';
-      
-      // Future compatible support for different messages for existing and nonexisting graphs
-      const seeGraphLinkHtml = buildGraphLinkHtml;
+      const paperTitleHtml = '<p class="connectedpapers-title">' + title + '</p>';
+
+      // Not enough references and citations parsed to build a graph
       const graphNotVisual = '<p>Seems like this paper is still not in our database. Please try again in a few' +
                              ' days.</p>';
 
@@ -77,54 +76,52 @@
         return 4294967296 * (2097151 & h2) + (h1>>>0);
       };
 
-      const selected_thumbnail_num = Math.abs(cyrb53(arxivId)) % NUMBER_OF_THUMBNAILS;
+      const selectedThumbnailNum = Math.abs(cyrb53(arxivId)) % NUMBER_OF_THUMBNAILS;
 
-      const chosenGraph = ARXIV_THUMBNAILS_ADDR + 'g' + selected_thumbnail_num + '.jpg';
-      const chosenGraphHtml = '<img src="' + chosenGraph + '" alt="Example graph image" width="140" height="120"' +
-                              ' class="connectedpapers-img">';
+      const chosenGraphThumbnail = ARXIV_THUMBNAILS_ADDR + 'g' + selectedThumbnailNum + '.jpg';
+      const chosenGraphThumbnailHtml = '<img src="' + chosenGraphThumbnail + '" alt="Example graph image" width="140" ' +
+                                       'height="120" class="connectedpapers-img">';
 
-      const infoLine = '<p class="connectedpapers-info-line">See related papers to:</p>';
+      const infoLine = '<p class="connectedpapers-description">See related papers to:</p>';
 
       const textDivOpen = '<div class="connectedpapers-text-cont">';
-      const buildGraphTextDiv = textDivOpen + infoLine + buildGraphLinkHtml + '</div>';
-      const seeGraphTextDiv = textDivOpen + infoLine + seeGraphLinkHtml + '</div>';
+      const buildGraphTextDiv = textDivOpen + infoLine + paperTitleHtml + '</div>';
 
       const buildGraphHtml = '<div class="connectedpapers-width-limiter"><a class="connectedpapers-link" href="' +
-                             graphUrl + '" target="_blank"><div class="connectedpapers-container">' + chosenGraphHtml +
-                             buildGraphTextDiv + '</div></a></div>';
+                             graphUrl + '" target="_blank"><div class="connectedpapers-container">' +
+                             chosenGraphThumbnailHtml + buildGraphTextDiv + '</div></a></div>';
 
-      const seeGraphHtml = '<div class="connectedpapers-width-limiter"><a class="connectedpapers-link" href="' +
-                           graphUrl + '" target="_blank"><div class="connectedpapers-container">' + chosenGraphHtml +
-                           seeGraphTextDiv + '</div></a></div>';
+      // Future compatability - different message for built graphs
+      const seeGraphHtml = buildGraphHtml;
 
       if (versionsResponse == null) {
         // Graph not yet built ever
-        $output.html(css_loader + buildGraphHtml);
+        $output.html(cssLoader + buildGraphHtml);
         return;
       }
       const versionsData = versionsResponse.result_dates;
       if (versionsData.length == 0) {
         // Graph not yet built ever
-        $output.html(css_loader + buildGraphHtml);
+        $output.html(cssLoader + buildGraphHtml);
         return;
       }
       const mostRecentVersion = versionsData[versionsData.length - 1];
       if (mostRecentVersion.visual) {
         // Graph already built, ready to be shown
-        $output.html(css_loader + seeGraphHtml);
+        $output.html(cssLoader + seeGraphHtml);
         return;
       }
       if (versionsResponse.rebuild_available) {
         // Graph non-available, but rebuild available
-        $output.html(css_loader + buildGraphHtml);
+        $output.html(cssLoader + buildGraphHtml);
         return;
       }
       // Graph non-available
       $output.html(graphNotVisual);
     }).fail(versionsResponse => {
-      $output.html(css_loader + communicationErrorHtml);
+      $output.html(cssLoader + communicationErrorHtml);
     })
   }).fail(translationResponse => {
-    $output.html(css_loader + communicationErrorHtml);
+    $output.html(cssLoader + communicationErrorHtml);
   });
 })();
