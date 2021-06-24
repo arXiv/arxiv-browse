064a046892fad69fda48c233b5ac0feaf75b1303
Brian D. Caruso <bdc34@cornell.edu>
2021-06-24 15:15:21 -0400

    Adding version handling for documents db implementation.

--- browse/services/documents/db_implementation/db_abs.py
+++ browse/services/documents/db_implementation/db_abs.py
@@ -1,12 +1,13 @@
 """Legacy DB backed core metadata service."""
 from typing import Dict, List, Optional
+from dataclasses import replace
 
 from browse.domain.metadata import DocMetadata
 from browse.domain.identifier import Identifier
 from browse.services.documents.config.deleted_papers import DELETED_PAPERS
 
 from browse.services.documents.base_documents import DocMetadataService, \
-    AbsDeletedException, AbsNotFoundException 
+    AbsDeletedException, AbsNotFoundException , AbsVersionNotFoundException
 
 from browse.services.database.models import Metadata
 
@@ -40,42 +41,48 @@ class DbDocMetadataService(DocMetadataService):
             raise AbsDeletedException(DELETED_PAPERS[paper_id.id])
 
         latest_version = self._abs_for_version(identifier=paper_id)
-        return latest_version
+        if not paper_id.has_version \
+           or paper_id.version == latest_version.version:
+            return replace(latest_version,
+                           is_definitive=True,
+                           is_latest=True)
 
-        #TODO deal with versions
-        # if not paper_id.has_version \
-        #    or paper_id.version == latest_version.version:
-        #     return dataclasses.replace(latest_version,
-        #                                is_definitive=True,
-        #                                is_latest=True)
-
-        # try:
-        #     this_version = self._abs_for_version(identifier=paper_id,
-        #                                          version=paper_id.version)
-        # except AbsNotFoundException as e:
-        #     if paper_id.is_old_id:
-        #         raise
-
-        #     raise AbsVersionNotFoundException(e) from e
+        try:
+            this_version = self._abs_for_version(identifier=paper_id,
+                                                 version=paper_id.version)
+        except AbsNotFoundException as e:
+            if paper_id.is_old_id:
+                raise
+            else:
+                raise AbsVersionNotFoundException(e) from e
 
         # Several fields need to reflect the latest version's data
-        # combined_version: DocMetadata = dataclasses.replace(
-        #     this_version,
-        #     version_history=latest_version.version_history,
-        #     categories=latest_version.categories,
-        #     primary_category=latest_version.primary_category,
-        #     secondary_categories=latest_version.secondary_categories,
-        #     primary_archive=latest_version.primary_archive,
-        #     primary_group=latest_version.primary_group,
-        #     is_definitive=True,
-        #     is_latest=False)
-
-        # return combined_version
+        combined_version: DocMetadata = replace(
+            this_version,
+            version_history=latest_version.version_history,
+            categories=latest_version.categories,
+            primary_category=latest_version.primary_category,
+            secondary_categories=latest_version.secondary_categories,
+            primary_archive=latest_version.primary_archive,
+            primary_group=latest_version.primary_group,
+            is_definitive=True,
+            is_latest=False)
+        return combined_version
 
 
     def _abs_for_version(self, identifier: Identifier,
                          version: Optional[int] = None) -> DocMetadata:
-        res = Metadata.query.filter( Metadata.paper_id.like( identifier.id ) ).first()
+        """Get a specific version of a paper's abstract metadata.
+
+        if version is None then get the latest version."""
+        if version:
+            res = (Metadata.query
+                   .filter( Metadata.paper_id == identifier.id)
+                   .filter( Metadata.version == identifier.version )).first()                   
+        else:
+            res = (Metadata.query
+                   .filter(Metadata.paper_id == identifier.id)
+                   .filter(Metadata.is_current == 1)).first()
         if not res:
             raise AbsNotFoundException()
         return to_docmeta(res)
