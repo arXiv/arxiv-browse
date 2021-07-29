9dd50fa57f3debfc9d33cd09c066f6ddae41ab6f
Brian D. Caruso <bdc34@cornell.edu>
2021-07-29 11:13:01 -0400

    Bumping arxiv-auth to 0.4.3rc4
    And change to use git tags as image tags if they exist.
    ARXIVNG-4354

--- deploy/build-and-push.sh
+++ deploy/build-and-push.sh
@@ -1,16 +1,18 @@
 set -e
 
 if [ -n "${GITHUB_SHA}" ] ; then
-    echo "Using GITHUB_SHA"
+    echo "Using GITHUB_SHA for image tag"
     GIT_V=${GITHUB_SHA}
 else
-    echo "Getting git version for HEAD"
-    GIT_V=$(git rev-parse --short HEAD)
+    echo "Getting git version for HEAD to use for image tag"
+    GIT_V=$(git describe --exact-match --tags 2> /dev/null || git rev-parse --short HEAD)
 fi
 
 TAG=gcr.io/$PROJ/browse:$GIT_V
+echo "Using tag $TAG"
 
 docker build --build-arg git_commit=${GIT_V} -t ${TAG} .
 docker push ${TAG}
 
 echo ${TAG} > TAG.txt
+echo "Build and pushed to $TAG"
