63e474b1cfe63d89436e6855a06688a086579cc5
Zeke Sikelianos <zeke@sikelianos.com>
2022-02-03 19:22:11 -0800

    tidy up

--- browse/static/js/replicate.js
+++ browse/static/js/replicate.js
@@ -1,37 +1,36 @@
 // Labs integration for displaying machine learning demos from replicate.com
 
 (function() {
+  // Get the arXiv paper ID from the URL, e.g. "2103.17249"
+  const arxivPaperId = window.location.pathname.split('/').reverse()[0]
+  if (!arxivPaperId) return
+
+  const replicateHost = "https://replicate.com"
+  const replicateApiUrl = `${replicateHost}/api/v1/models?arxiv_paper_id=${arxivPaperId}`
   const container = document.getElementById("replicate-output")
 
   // This script is run whenever the Labs toggle input is clicked, regardless of
-  // of whether the toggle is enabled or disabled. If content already exists, 
+  // whether the toggle is enabled or disabled. If content already exists, 
   // we know the toggle was just disabled, so remove content and hide container.
   if (container.innerHTML.trim().length) {
     container.innerHTML = ""
-    container.setAttribute("style", "display:none");
+    container.setAttribute("style", "display:none")
     return
   } else {
-    container.setAttribute("style", "display:block");
+    container.setAttribute("style", "display:block")
   }
 
-  // Get the arXiv Paper ID from the URL
-  // const arxivPaperId = window.location.pathname.split('/').reverse()[0]
-  const arxivPaperId = "2103.17249"
-  const replicateHost = "https://replicate.com"
-  const replicateApiUrl = `${replicateHost}/api/v1/models?arxiv_paper_id=${arxivPaperId}`
-
-  // Find Replicate models for this paper
-  fetch(replicateApiUrl)
-      .then(response => {
-      if (response.ok) {
-          return response.json()
-      } else {
-          console.error("Unable to fetch model data from replicate.com")
-          return Promise.reject(response.status)
-      }
+  // Search the Replicate API for models that implement this paper
+  fetch(replicateApiUrl).then(response => {
+    if (response.ok) {
+        return response.json()
+    } else {
+        console.error(`Unable to fetch model data from ${replicateHost}`)
+        return Promise.reject(response.status)
+    }
   })
-      .then(data => render(data))
-      .catch(error => console.log(error))
+    .then(data => render(data))
+    .catch(error => console.log(error))
 
   // Generate HTML, sanitize it to prevent XSS, and inject into the DOM
   function render({ models }) {
@@ -41,10 +40,10 @@
       `)
   }
 
-  function noModelsFound(data) {
-    if (data.models?.length === 0) {
+  function noModelsFound(models) {
+    if (models.length === 0) {
       return `<p>
-        No demos found for this paper. You can <a href="https://replicate.com/docs">add one here</a>.
+        No demos found for this article. You can <a href="https://replicate.com/docs">add one here</a>.
       </p>`
     } else {
       return ``
