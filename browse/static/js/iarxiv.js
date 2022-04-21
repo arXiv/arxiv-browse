36a30bd4783b63c568e3b9d9332e0b8fdbbe6903
SBBCornell <sbb226@cornell.edu>
2022-04-21 14:00:53 -0400

    more prominent IArxiv link

--- browse/static/js/iarxiv.js
+++ browse/static/js/iarxiv.js
@@ -9,10 +9,11 @@
         <div style="clear:both"></div>
         <h3>IArxiv</h3>
         <div class="iarxiv">
-            <a href="/iarxiv_login">Signup</a>
-            with IArxiv.org to receive paper recommendations by email for 
-            astro-ph, hep-ph, hep-th and gr-qc. 
+            <a href="/iarxiv_login">Signup</a> with IArxiv.org to receive by email the daily Arxiv papers
+            sorted according to your preferences in astro-ph, hep-ph, hep-th and gr-qc.
             Expanding to all subject categories soon!
+            <br><br>
+            <a href="/iarxiv_login" class="button-fancy">Signup with IArxiv <span></span></a>
         <div>
     `);
 })();
