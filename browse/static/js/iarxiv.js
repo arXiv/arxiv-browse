280ac8970b13599ff6af0c303fb61759a9c0375c
SBBCornell <sbb226@cornell.edu>
2022-04-21 16:15:23 -0400

    minor text change

--- browse/static/js/iarxiv.js
+++ browse/static/js/iarxiv.js
@@ -9,7 +9,7 @@
         <div style="clear:both"></div>
         <h3>IArxiv</h3>
         <div class="iarxiv">
-            <a href="/iarxiv_login">Signup</a> with IArxiv.org to receive by email the daily Arxiv papers
+            <a href="/iarxiv_login">Signup</a> with IArxiv.org to receive, by email, the daily Arxiv papers
             sorted according to your preferences in astro-ph, hep-ph, hep-th and gr-qc.
             Expanding to all subject categories soon!
             <br><br>
