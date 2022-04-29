6a180d56f0758b2f4df0626f3b73e5f02527c8e9
Ali Abid <aliabid94@gmail.com>
2022-04-29 00:30:13 +0000

    connect with HF Api, use tailwind colors for model cards

--- browse/static/js/spaces.js
+++ browse/static/js/spaces.js
@@ -23,22 +23,46 @@
   const arxivPaperId = params.get("override_paper_id") || window.location.pathname.split('/').reverse()[0]
   if (!arxivPaperId) return
 
-  const huggingfaceHost = "https://huggingface.co"
-  const spacesHost = `${huggingfaceHost}/spaces`
-  // const spacesApiUrl = `${spacesHost}/api/models?filter=arxiv:${arxivPaperId}`
-  const spacesApiUrl = `${huggingfaceHost}/api/spaces/flax-community/dalle-mini`
+  const huggingfaceApiHost = "https://huggingface.co/api";
+  const huggingfaceSpacesHost = "https://huggingface.co/spaces";
+  const huggingfaceModelsFromPaperApi = `${huggingfaceApiHost}/models?filter=arxiv:${arxivPaperId}`;
 
   // Search the HF Spaces API for models that implement this paper
-  fetch(spacesApiUrl).then(response => {
-    if (response.ok) {
-      return response.json()
-    } else {
-      console.error(`Unable to fetch model data from ${spacesHost}`)
-      return Promise.reject(response.status)
+
+  (async () => {
+    let response = await fetch(huggingfaceModelsFromPaperApi);
+    if (!response.ok) {
+      console.error(`Unable to fetch model data from ${huggingfaceModelsFromPaperApi}`)
+      render([]);
+      return;
     }
-  })
-    .then(data => render([data]))
-    .catch(error => console.log(error))
+    let models = await response.json();
+    if (models.length === 0) {
+      render([]);
+      return;
+    }
+    const model_ids = models.map(m => m.id).join(",");
+    const huggingfaceSpacesFromModelsApi = `${huggingfaceApiHost}/spaces?models=${model_ids}`;
+    const response = await fetch(huggingfaceSpacesFromModelsApi);
+    if (!response.ok) {
+      console.error(`Unable to fetch spaces data from ${huggingfaceSpacesFromModelsApi}`)
+      render([]);
+      return;
+    }
+    const spaces = await response.json();
+    let spaces_data = [];
+    await Promise.all(spaces.map(async (space) => {
+      const huggingfaceSpaceApi = `${huggingfaceApiHost}/spaces/${space.id}`
+      const response = await fetch(huggingfaceSpaceApi);
+      if (!response.ok) {
+        console.error(`Unable to fetch data from ${huggingfaceSpaceApi}`);
+        return;
+      }
+      let space_data = await response.json();
+      spaces_data.push(space_data);
+    }));
+    render(spaces_data);
+  })()
 
   // Generate HTML, sanitize it to prevent XSS, and inject into the DOM
   function render(models) {
@@ -73,20 +97,50 @@
   }
 
   function renderModel(model) {
-    console.log(model)
+    const tailwind_color_map = {
+      slate: "#475569",
+      gray: "#4b5563",
+      zinc: "#52525b",
+      neutral: "#525252",
+      stone: "#57534e",
+      red: "#dc2626",
+      orange: "#ea580c",
+      amber: "#d97706",
+      yellow: "#ca8a04",
+      lime: "#65a30d",
+      green: "#16a34a",
+      emerald: "#059669",
+      teal: "#0d9488",
+      cyan: "#0891b2",
+      sky: "#0284c7",
+      blue: "#2563eb",
+      indigo: "#4f46e5",
+      violet: "#7c3aed",
+      purple: "#9333ea",
+      fuchsia: "#475569",
+      pink: "#c026d3",
+      rose: "#e11d48"
+    };
+    const getColor = (color) => {
+      if (color in tailwind_color_map) {
+        return tailwind_color_map[color];
+      } else {
+        return tailwind_color_map["gray"];
+      }
+    }
     return `
       <div class="spaces-model">
-        <a class="spaces-card" href="${spacesHost}/${model.id}" 
-          style="background: -webkit-linear-gradient(315deg,${model.cardData.colorFrom},${model.cardData.colorTo}); background: linear-gradient(315deg,${model.cardData.colorFrom},${model.cardData.colorTo});"
+        <a class="spaces-card" href="${huggingfaceSpacesHost}/${model.id}" 
+          style="background: -webkit-linear-gradient(315deg,${getColor(model.cardData.colorFrom)},${getColor(model.cardData.colorTo)}); background: linear-gradient(315deg,${getColor(model.cardData.colorFrom)},${getColor(model.cardData.colorTo)});"
         >
           <span class="spaces-emoji">${model.cardData.emoji}</span>
           <span class="spaces-title">${model.cardData.title}</span>
         </a>
         <div class="spaces-model-details">
-          <a href="${spacesHost}/${model.id}">
+          <a href="${huggingfaceSpacesHost}/${model.id}">
             <h3 class="spaces-model-details-heading">${model.id}</h3>
           </a>
-          <p>A ${model.cardData.sdk} demo</p>
+          <p class="spaces-model-sdk">A ${model.cardData.sdk} demo.</p>
         </div>
       </div>
     `
