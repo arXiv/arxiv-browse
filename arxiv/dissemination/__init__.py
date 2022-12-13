983810cfaa5680fc49dcdcd9798c4e36989b7627
Brian D. Caruso <bdc34@cornell.edu>
2022-12-13 16:50:28 -0500

    Changes to use cloudpathlib

--- arxiv/dissemination/__init__.py
+++ arxiv/dissemination/__init__.py
@@ -4,27 +4,33 @@ import os
 
 from flask import Flask
 
-from google.cloud import storage
-
 from .routes import blueprint
+
+from cloudpathlib.anypath import to_anypath
 
 import logging
 logger = logging.getLogger(__file__)
 
 #################### config ####################
-bucket_name = os.environ.get('BUCKET','arxiv-production-data')
-"""Name of the GS bucket. Must not have the gs:// prefix"""
+storage_prefix = os.environ.get('STORAGE_PREFIX','gs://arxiv-production-data/ps_cache')
+"""Storage prefix to use. Ex gs://arxiv-production-data/ps_cache
 
-path_prefix = os.environ.get('PATH_PREFIX', 'ps_cache')
+Use something like /cache/ps_cache for a file system.
+
+Use something like ./testing/ps_cahe for testing data.
+
+Should not end with a /.
+"""
 
 chunk_size = int(os.environ.get('CHUNK_SIZE', 1024 * 256))
 """chunk size from GS. Bytes. Must be mutiples of 256k"""
 
 trace = bool(os.environ.get('TRACE', '1') == '1')
+"""To activate Google logging and trace.
 
+On by default,anything other than 1 deactivates.
+"""
 #################### App ####################
-gs_client = storage.Client()
-
 from flask.logging import default_handler
 root = logging.getLogger()
 root.addHandler(default_handler)
@@ -32,32 +38,20 @@ root.addHandler(default_handler)
 problems = []
 if chunk_size % (1024 *256):
     problems.append('CHUNK_SIZE must be a multiple of 256kb.')
-if path_prefix.startswith('/'):
-    problems.append(f'PATH_PREFIX should not start with a slash, using keys not gs:// URLs, prefix was {path_prefix}')
-if not bucket_name:
-    problems.append('Must set BUCKET.')
-    if bucket_name.startswith('gs://'):
-        problems.append('BUCKET must not start with gs://, use just the bucket name.')
-    if not gs_client.bucket(bucket_name).exists():
-        problems.append('BUCKET {BUCKET} does not exist or cannot read.')
-        problems.append('Using service account {gs_client.get_service_account_email()}.')
+if storage_prefix.endswith('/'):
+    problems.append(f'STORAGE_PREFIX should not end with a slash, prefix was {path_prefix}')
+if not to_anypath(storage_prefix).exists():
+        problems.append('BUCKET {STORAGE_PREFIX} does not exist or cannot read.')
 if problems:
     [logger.error(prob) for prob in problems]
     exit(1)
 
-bucket = gs_client.bucket(bucket_name)
-
 app = Flask(__name__)
-
 app.config.update(
-    path_prefix = path_prefix,
+    storage_prefix=storage_prefix,
     chunk_size=chunk_size,
-    gs_client=gs_client,
-    bucket=bucket,
     )
-
 app.register_blueprint(blueprint)
-
 
 ############### trace and logging setup ###############
 if trace:
@@ -74,7 +68,6 @@ if trace:
 
     # https://cloud.google.com/trace/docs/setup/python-ot#initialize_flask
     set_global_textmap(CloudTraceFormatPropagator())
-
     tracer_provider = TracerProvider()
     cloud_trace_exporter = CloudTraceSpanExporter()
     tracer_provider.add_span_processor(
@@ -82,7 +75,6 @@ if trace:
     )
     trace.set_tracer_provider(tracer_provider)
     tracer = trace.get_tracer(__name__)
-
     FlaskInstrumentor().instrument_app(app)
 else:
     logger.warn("No setup of google cloud trace and logging")
--- arxiv/dissemination/routes.py
+++ arxiv/dissemination/routes.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 from email.utils import format_datetime
 import logging
 
@@ -5,14 +7,18 @@ from flask import abort, make_response, request, Blueprint, current_app
 
 from arxiv.identifier import IdentifierException, Identifier
 
+from cloudpathlib.anypath import to_anypath
+
 logger = logging.getLogger(__file__)
 
 blueprint = Blueprint('routes',__name__)
 
 @blueprint.route("/status")
 def status():
-    # TODO Check we can read from the bucket
-    return {"status": "good"}
+    if current_app.config['storage'].exists():
+        return {"status": "good"}
+    else:
+        abort(500, "could not read from storage")
 
 
 @blueprint.route("/pdf/<string:category>/<string:arxiv_id>", methods=['GET', 'HEAD'])
@@ -21,11 +27,6 @@ def serve_legacy_id_pdf(category: str, arxiv_id: str):
     """Serve PDFs for legacy IDs"""
     return serve_pdf( f"{category}/{arxiv_id}")
 
-
-
-def gs_key_for_id(id: Identifier, format: str) -> str:
-    archive = id.archive if id.is_old_id else 'arxiv'
-    return f"{current_app.config['path_prefix']}/{archive}/{format}/{id.yymm}/{id.filename}v{id.version}.pdf"
 
 
 @blueprint.route("/pdf/<string:arxiv_id>", methods=['GET', 'HEAD'])
@@ -62,19 +63,18 @@ def serve_pdf(arxiv_id: str):
     except IdentifierException as ex:
         abort(400, description=ex)
 
-    key = gs_key_for_id(id, 'pdf')
-    logger.debug(f"looking for key {key}")
-
-    bucket = current_app.config['bucket']
-    blob = bucket.get_blob(key, chunk_size=chunk_size)
-    if not blob: abort(404)
+    format = 'pdf'
+    archive = id.archive if id.is_old_id else 'arxiv'
+    item = to_anypath(f"{current_app.config['storage_prefix']}/{archive}/{format}/{id.yymm}/{id.filename}v{id.version}.pdf")
+    logger.debug(f"looking for key {item}")
+    if not item.exists() : abort(404)
 
     if request.method == 'GET':
         def stream():
-            with blob.open('rb') as gsf:
+            with item.open('rb') as fh:
                 done = False
-                while (not done and gsf.readable() and not gsf.closed):
-                    bytes = gsf.read(chunk_size)
+                while (not done and fh.readable() and not fh.closed):
+                    bytes = fh.read(chunk_size)
                     if bytes:
                         yield bytes
                     else:
@@ -84,21 +84,12 @@ def serve_pdf(arxiv_id: str):
     else:
         resp = make_response('')
 
-    # TODO arxiv.org etag is very different do we need to fix or is using the gs value fine?
-    resp.headers.set('ETag', blob.etag)
-
-    resp.headers.set('Content-Type', 'application/pdf')
-    resp.headers.set('Last-Modified', format_datetime(blob.updated))
-    resp.headers.set('Content-Length', blob.size)
     resp.headers.set('Access-Control-Allow-Origin', '*')
-
-    if blob.content_encoding:
-        resp.headers.set('Content-Encoding', blob.content_encoding)
-    if blob.content_disposition:
-        resp.headers.set('Content-Disposition', blob.content_disposition)
-    if blob.cache_control:
-        resp.headers.set('Cache-Control', blob.cache_control)
-    if blob.content_language:
-        resp.headers.set('Content-Language', blob.content_language)
+    resp.headers.set('Content-Type', 'application/pdf')
+    stat = item.stat()
+    resp.headers.set('Last-Modified', format_datetime(datetime.fromtimestamp(stat.st_mtime, tz=timezone.utc)))
+    resp.headers.set('Content-Length', stat.st_size)
+    if hasattr(item, 'etag'): # TODO arxiv.org etag is very different do we need to fix or is using the gs value fine?
+        resp.headers.set('ETag', item.etag)
 
     return resp
