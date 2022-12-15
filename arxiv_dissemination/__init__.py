2c3c947a232d6f5179627074ffdbd87d0e02f40e
Brian D. Caruso <bdc34@cornell.edu>
2022-12-15 13:37:01 -0500

    Adds integration test

--- arxiv_dissemination/__init__.py
+++ arxiv_dissemination/__init__.py
@@ -9,6 +9,12 @@ from .trace import setup_trace
 from cloudpathlib.anypath import to_anypath
 
 import logging
+logging.basicConfig(level=logging.INFO)
+
+from flask.logging import default_handler
+root = logging.getLogger()
+root.addHandler(default_handler)
+
 logger = logging.getLogger(__file__)
 
 """Type for Path that is either a cloud or local path."""
@@ -27,16 +33,14 @@ Should not end with a /.
 chunk_size = int(os.environ.get('CHUNK_SIZE', 1024 * 256))
 """chunk size from GS. Bytes. Must be mutiples of 256k"""
 
-trace = bool(os.environ.get('TRACE', '0') == '1')
+trace = False # bool(os.environ.get('TRACE', '0') == '1')
 """To activate Google logging and trace.
 
 Off by default, set to 1 to activate.
 """
-#################### App ####################
 
-from flask.logging import default_handler
-root = logging.getLogger()
-root.addHandler(default_handler)
+#################### App ####################
+app = Flask(__name__)
 
 problems = []
 if chunk_size % (1024 *256):
@@ -46,14 +50,17 @@ if storage_prefix.endswith('/'):
 if not to_anypath(storage_prefix).exists():
         problems.append('BUCKET {STORAGE_PREFIX} does not exist or cannot read.')
 if problems:
-    [logger.error(prob) for prob in problems]
+    [app.logger.error(prob) for prob in problems]
     exit(1)
 
-app = Flask(__name__)
+app.logger.info(f"trace is {trace}")
+app.logger.info(f"storage_prefix is {storage_prefix}")
+
 app.config.update(
     storage_prefix=storage_prefix,
     chunk_size=chunk_size,
     )
+
 app.register_blueprint(blueprint)
 
 ############### trace and logging setup ###############
