74705e1285e4acd4bc2c2f1f4b2695adee6f8747
Brian D. Caruso <bdc34@cornell.edu>
2023-06-29 13:41:36 -0400

    Fixes columns for AuthorIds and OrcidIds for use with sqlite

--- browse/services/database/models.py
+++ browse/services/database/models.py
@@ -512,7 +512,7 @@ class AuthorIds(db.Model):
 
     user_id = Column(ForeignKey('tapir_users.user_id'), primary_key=True)
     author_id = Column(String(50), nullable=False, index=True)
-    updated = Column(TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
+    updated = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
 
     user = relationship('User', uselist=False)
 
@@ -521,8 +521,8 @@ class OrcidIds(db.Model):
 
     user_id = Column(ForeignKey('tapir_users.user_id'), primary_key=True)
     orcid = Column(String(19), nullable=False, index=True)
-    authenticated = Column(TINYINT(1), nullable=False, server_default=text("'0'"))
-    updated = Column(TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
+    authenticated = Column(Integer, nullable=False, server_default=text("'0'"))
+    updated = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
 
     user = relationship('User', uselist=False)
 
