d9b8ff81e70616661f0ff6cccedf6efe13f9ca9e
Mark Nazzaro <marknazzaro2@gmail.com>
2023-07-17 10:41:29 -0400

    Change latexml base url

--- browse/formatting/latexml.py
+++ browse/formatting/latexml.py
@@ -6,7 +6,7 @@ from browse.services.database import get_latexml_status_for_document
 import logging
 
 def get_latexml_url (article: DocMetadata) -> Optional[str]:
-    LATEXML_URI_BASE = "https://services.dev.arxiv.org/html"
+    LATEXML_URI_BASE = "https://browse.arxiv.org/latexml"
     status = get_latexml_status_for_document(article.arxiv_id, article.version)
     path = f'{article.arxiv_id}v{article.version}/{article.arxiv_id}v{article.version}.html'
     return f'{LATEXML_URI_BASE}/{path}' if status == 1 else None
