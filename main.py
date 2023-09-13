e69da876241675332e4a7ff33799a1c4f65f83d2
Brian D. Caruso <bdc34@cornell.edu>
2023-09-13 12:07:07 -0400

    Set TEMPLATES_AUTO_RELOAD in main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 """Simple file to run arxiv-browse in debug mode.
 
 Run as `python main.py`"""
+import os
 from browse.factory import create_web_app
 
 
 if __name__ == "__main__":
+    os.environ['TEMPLATES_AUTO_RELOAD'] = "1"
+    os.environ['DEBUG'] = "1"
     app = create_web_app()
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.config['DEBUG'] = True
     app.run(debug=True, port=8080)
