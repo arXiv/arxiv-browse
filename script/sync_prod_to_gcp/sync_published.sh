326b78325db486b91529af34686a966b5da99057
Naoyuki Tai <ntai@arxiv.org>
2023-10-03 16:12:42 -0400

    Separating out the text logging so that the cron job can run from non-log directory.

--- script/sync_prod_to_gcp/sync_published.sh
+++ script/sync_prod_to_gcp/sync_published.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+TEXT_LOG_DIR=/opt_arxiv/e-prints/dissemination/sync_prod_to_gcp
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 if [ ! -d sync.venv ] ; then
   make
 fi
-
-echo $1
 
 # Is this a test/
 if [ "$1" = "YES_THIS_IS_A_TEST" ]
@@ -13,7 +12,6 @@ then
     shift
     TESTING_ARGS="-v -d --test"
 fi
-
 
 # if running between 8pm and midnight
 DATE=`date +%y%m%d --date='12:00 tomorrow'`
@@ -33,43 +31,42 @@ then
 fi
 
 JSON_LOG_DIR=/opt_arxiv/e-prints/logs/sync
-mkdir -p /opt_arxiv/e-prints/logs/sync
+mkdir -p $JSON_LOG_DIR
 
 . sync.venv/bin/activate
 export GOOGLE_APPLICATION_CREDENTIALS=~/arxiv-production-cred.json
-python sync_published_to_gcp.py $TESTING_ARGS --json-log-dir $JSON_LOG_DIR  /data/new/logs/publish_$DATE.log > sync_published_$DATE.report 2> sync_published_$DATE.err
+python sync_published_to_gcp.py $TESTING_ARGS --json-log-dir $JSON_LOG_DIR  /data/new/logs/publish_$DATE.log > $TEXT_LOG_DIR/sync_published_$DATE.report 2> $TEXT_LOG_DIR/sync_published_$DATE.err
 deactivate
 
 if [ ! -z "$TESTING_ARGS" ]; then 
   exit 0
 fi
 
-if [ -s sync_published_$DATE.report ]
+if [ -s $TEXT_LOG_DIR/sync_published_$DATE.report ]
 then    
     if /bin/gsutil -q -h "Content-Type:text/plain" cp \
-	sync_published_$DATE.report gs://legacy_support/sync_published/$DATE.report ;
+	$TEXT_LOG_DIR/sync_published_$DATE.report gs://legacy_support/sync_published/$DATE.report ;
     then
-	rm sync_published_$DATE.report
+	rm -f $TEXT_LOG_DIR/sync_published_$DATE.report
     else
 	echo "Could not save report to GS! leaving on disk"
     fi
 else
-    echo "sync_published_to_gcp upload report was zero size" >> sync_published_$DATE.err
+    echo "sync_published_to_gcp upload report was zero size" >> $TEXT_LOG_DIR/sync_published_$DATE.err
 fi
 
-if [ -s sync_published_$DATE.err ]
+if [ -s $TEXT_LOG_DIR/sync_published_$DATE.err ]
 then
-    head -n 100 sync_published_$DATE.err
+    head -n 100 $TEXT_LOG_DIR/sync_published_$DATE.err
 
     if /bin/gsutil -q -h "Content-Type:text/plain" cp \
-	sync_published_$DATE.err gs://legacy_support/sync_published/$DATE.err ;
+	$TEXT_LOG_DIR/sync_published_$DATE.err gs://legacy_support/sync_published/$DATE.err ;
     then 
 	printf "\n\nError log saved at gs://legacy_support/sync_published/$DATE.err\n"
-	rm sync_published_$DATE.err
+	rm $TEXT_LOG_DIR/sync_published_$DATE.err
     else
 	echo "Could not save error report! leaving on disk"
     fi
 
     exit 1
 fi
-
