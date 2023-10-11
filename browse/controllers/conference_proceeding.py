773d12897d180482025f23630ac97f0950b8274d
kyokukou <erin@arxiv.org>
2023-10-11 13:57:49 -0700

    processing tar files works so long as they only contain html files

--- browse/controllers/conference_proceeding.py
+++ browse/controllers/conference_proceeding.py
@@ -25,36 +25,61 @@ def post_process_conference (name: str, bucket_name: str) -> Tuple[Dict[str, Opt
         logger.error('Error getting file from GCP',exc_info=True)
         return ex, 400
 
-    #get all the interior files
+    
     html_files=[]
     other_files=[]
-    if ungzipped_file.name.endswith(".tar"):
-        print("panic!")
-    else:
-        html_files.append(ungzipped_file)
+
+
+    if ungzipped_file.name.endswith(".tar"): #get all interior files from tar
+        with ungzipped_file.open() as data:           
+            raw_data=data.read()
+            tar_bytes=io.BytesIO(raw_data)
+
+            with tarfile.open(fileobj=tar_bytes, mode='r') as tar: #open tar file from byte string
+                file_list=tar.getnames()
+                for file_info in tar:
+                    if file_info.name.endswith(".html"):
+                        html_files.append(
+                            {"file":tar.extractfile(file_info).read(),
+                             "name":file_info.name})
+                    else:
+                        other_files.append(
+                            {"file":tar.extractfile(file_info),
+                             "name":file_info.name})
+
+    else: #get single html file
+        try:
+            with ungzipped_file.open() as data: 
+                raw_data=data.read()
+                html_files.append({"name":data.name, "file":raw_data} )
+        except Exception as ex:
+            logger.error('Error opening file',exc_info=True)
+            return ex, 400
 
     tar_buffer = io.BytesIO()
     with tarfile.open(fileobj=tar_buffer, mode="w:gz") as tar:
         #process and add each html file
-        for file in html_files:
-            processed=post_process_conference_file(file)
-            html_file_name=file.name
+        for entry in html_files:
+            #read and process data
+            text_html=entry["file"].decode('utf-8')
+            processed=post_process_html(text_html)
+
+            #put back into tar
+            html_file_name=entry["name"]      
             html_file = io.BytesIO(processed.encode('utf-8'))
             tarinfo = tarfile.TarInfo(html_file_name)
             tarinfo.size = len(processed)
             tar.addfile(tarinfo, html_file)
 
-        #add back other files
-        for file in other_files:
-            tarinfo = tarfile.TarInfo(file.name)
-            tarinfo.size = file.size
-            tar.addfile(tarinfo, html_file)
+        #add back non html files
+        for entry in other_files:
+            tarinfo = tarfile.TarInfo(entry["name"])
+            tarinfo.size = entry["file"].size
+            tar.addfile(tarinfo, entry["file"])
 
 
     tar_buffer.seek(0)
     blob_name=name.replace(".html.gz",".tar.gz")
-    if blob_name.startswith("ftp/"):
-        blob_name=blob_name[4:]
 
     #upload to GCP
     try:
@@ -65,19 +90,12 @@ def post_process_conference (name: str, bucket_name: str) -> Tuple[Dict[str, Opt
         logger.error('Error sending file to GCP',exc_info=True)
         return ex, 400
 
-    response_data: Dict[str, Any] = {}
-    response_data['result'] = "success"
-    return response_data, 200, 
+    return {"result":"success"}, 200 
 
 
 def post_process_conference_file (file: UngzippedFileObj) -> str: 
 #called on each html file in conference proceedings to be processed
-    try:
-        with file.open() as data: 
-            rawdata=data.read()
-    except Exception as ex:
-        logger.error('Error opening file',exc_info=True)
-        return ex, 400
+
 
     text_html=rawdata.decode('utf-8')
     processed_html=post_process_html(text_html)
