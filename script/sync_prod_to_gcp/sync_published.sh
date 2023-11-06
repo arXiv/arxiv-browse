447d2c74043fe7c3445fd12e22c76a6e88ed5d4c
Naoyuki Tai <ntai@arxiv.org>
2023-11-06 17:07:33 -0500

    Tai - checking in BrianC's changes.

--- script/sync_prod_to_gcp/sync_published.sh
+++ script/sync_prod_to_gcp/sync_published.sh
@@ -64,7 +64,7 @@ mkdir -p $JSON_LOG_DIR
 
 . sync.venv/bin/activate
 export GOOGLE_APPLICATION_CREDENTIALS=~/arxiv-production-cred.json
-python sync_published_to_gcp.py $TESTING_ARGS --json-log-dir $JSON_LOG_DIR  /data/new/logs/publish_$DATE.log >> $TEXT_LOG_DIR/sync_published_$DATE.report 2>> $TEXT_LOG_DIR/sync_published_$DATE.err
+python sync_published_to_gcp.py $TESTING_ARGS -v --json-log-dir $JSON_LOG_DIR  /data/new/logs/publish_$DATE.log >> $TEXT_LOG_DIR/sync_published_$DATE.report 2>> $TEXT_LOG_DIR/sync_published_$DATE.err
 deactivate
 
 if [ ! -z "$TESTING_ARGS" ]; then 
