cb9912d5d6aa14d8f200bd72156d9d27dccea5eb
SBBCornell <sbb226@cornell.edu>
2023-11-21 16:57:08 -0500

    desktop and mobile header style fixes

--- browse/static/js/addons.js
+++ browse/static/js/addons.js
@@ -21,28 +21,26 @@ let create_header = () => {
     let id = window.location.pathname.split('/')[2];
 
     var LogoBanner = `
-    <div style="display: flex; width: 60%;">
-        <a href="https://arxiv.org/" style="text-decoration: none; width:80px">
-            <img alt="logo" class="logo" role="presentation" style="background-color: transparent;" src="https://services.dev.arxiv.org/html/static/arxiv-logo-one-color-white.svg">
-            <img alt="logo" class="logomark" role="presentation" style="background-color: transparent;" src="https://services.dev.arxiv.org/html/static/arxiv-logomark-small-white.svg">
-        </a>
-        <div class="header-message" role="banner" style="padding-left: 15px; padding-top: 5px;">
-            ${id === 'submission' ? 'This is <strong>Experimental HTML</strong>. By design, HTML will not look exactly like the PDF. We invite you to report any errors that don\'t represent the intent or meaning of your paper. <span class="sr-only">Use Alt+Y to toggle on accessible reporting links and Alt+Shift+Y to toggle off.</span><a href=https://github.com/brucemiller/LaTeXML/wiki/Porting-LaTeX-packages-for-LaTeXML target="_blank">View supported LaTeX packages</a> and <a href=https://github.com/brucemiller/LaTeXML/issues target="_blank">help improve conversions</a>.' :
-            'This is <strong>Experimental HTML</strong>. We invite you to report rendering errors. <span class="sr-only">Use Alt+Y to toggle on accessible reporting links and Alt+Shift+Y to toggle off.</span> Learn more <a href="https://info.arxiv.org/about/accessible_HTML.html" target="_blank">about this project</a> and <a href=https://github.com/brucemiller/LaTeXML/issues target="_blank">help improve conversions</a>.'}
-        </div>
+    <div class="html-header-logo">
+      <a href="https://arxiv.org/">
+          <img alt="logo" class="logo" role="presentation" width="100" src="https://services.dev.arxiv.org/html/static/arxiv-logo-one-color-white.svg">
+      </a>
+    </div>
+    <div class="html-header-message" role="banner">
+        <p>${id === 'submission' ? 'This is <strong>Experimental HTML</strong>. By design, HTML will not look exactly like the PDF. We invite you to report any errors that don\'t represent the intent or meaning of your paper. <span class="sr-only">Use Alt+Y to toggle on accessible reporting links and Alt+Shift+Y to toggle off.</span><a href=https://github.com/brucemiller/LaTeXML/wiki/Porting-LaTeX-packages-for-LaTeXML target="_blank">View supported LaTeX packages</a> and <a href=https://github.com/brucemiller/LaTeXML/issues target="_blank">help improve conversions</a>.' :
+        'This is <strong>Experimental HTML</strong>. We invite you to report rendering errors. <span class="sr-only">Use Alt+Y to toggle on accessible reporting links and Alt+Shift+Y to toggle off.</span> Learn more <a href="https://info.arxiv.org/about/accessible_HTML.html" target="_blank">about this project</a> and <a href=https://github.com/brucemiller/LaTeXML/issues target="_blank">help improve conversions</a>.'}
+        </p>
     </div>`;
 
-
     var Links = `
-        <div style="display: inline-flex; align-items: center;">
-            <a class="ar5iv-footer-button hover-effect" style="color: white;" href="https://info.arxiv.org/about/accessible_HTML.html" target="_blank">Why HTML?</a>
-            <a class="ar5iv-footer-button hover-effect" target="_blank" style="color: white;" href="#myForm" onclick="event.preventDefault(); var modal = document.getElementById('myForm'); modal.style.display = 'block'; bugReportState.setInitiateWay('Header');">Report Issue</a>
-            ${id === 'submission' ? '' : `<a class="ar5iv-footer-button hover-effect" style="color: white;" href="https://arxiv.org/abs/${window.location.href.match(/https:\/\/.+\/html\/(.+)/)[1]}">Back to Abstract</a>`}
-
-            <a class="ar5iv-toggle-color-scheme" href="javascript:toggleColorScheme()" title="Toggle ar5iv color scheme" style="float: right;">
+        <nav class="html-header-nav">
+            <a class="ar5iv-footer-button hover-effect" href="https://info.arxiv.org/about/accessible_HTML.html" target="_blank">Why HTML?</a>
+            <a class="ar5iv-footer-button hover-effect" target="_blank" href="#myForm" onclick="event.preventDefault(); var modal = document.getElementById('myForm'); modal.style.display = 'block'; bugReportState.setInitiateWay('Header');">Report Issue</a>
+            ${id === 'submission' ? '' : `<a class="ar5iv-footer-button hover-effect" href="https://arxiv.org/abs/${window.location.href.match(/https:\/\/.+\/html\/(.+)/)[1]}">Back to Abstract</a>`}
+            <a class="ar5iv-toggle-color-scheme" href="javascript:toggleColorScheme()" title="Toggle dark/light mode">
                 <span class="color-scheme-icon"></span>
             </a>
-        </div>`;
+        </nav>`;
 
     desktop_header.innerHTML = LogoBanner + Links;
     desktop_header.classList.add('desktop_header');
@@ -55,41 +53,40 @@ let create_mobile_header = () => {
     let id = window.location.pathname.split('/')[2];
 
     var mobile_header= `
-    <div class="container-fluid">
-    <a class="navbar-brand" href="https://arxiv.org/" style="text-decoration: none; width:80px">
-      <img alt="logo" class="logo" role="presentation" style="background-color: transparent;"
-        src="https://services.dev.arxiv.org/html/static/arxiv-logo-one-color-white.svg">
-      <img alt="logo" class="logomark" role="presentation" style="background-color: transparent; margin-left:40px;"
+    <div class="html-header-logo">
+      <a href="https://arxiv.org/">
+        <img alt="logo" class="logomark" role="presentation"
         src="https://services.dev.arxiv.org/html/static/arxiv-logomark-small-white.svg">
-    </a>
-        <!--toc button-->
-        <div class='subcontainer-fluid'>
-          <button class="navbar-toggler ar5iv-footer-button" type="button" data-bs-theme="dark" data-bs-toggle="collapse" aria-expanded="false"
-            data-bs-target=".ltx_page_main >.ltx_TOC.mobile" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation" style="border:none; margin-right: 0em;">
-            <span class="navbar-toggler-icon" style="width:1em;height:1em;margin-top: 0.1em;"></span>
-          </button>
-          <!--back to abstract-->
-          ${id === 'submission' ? '' : `
-          <!-- <a class="nav-link ar5iv-footer-button hover-effect" style="color: white; display:inline-flex; flex-direction: column; align-items:center; text-align:center" href="#"> -->
-            <a class="nav-link ar5iv-footer-button hover-effect" style="color: white; margin-right:0em;" href="https://arxiv.org/abs/${window.location.href.match(/https:\/\/.+\/html\/(.+)/)[1]}">
-            <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512" style="background-color:transparent; z-index:2">
-                <style>svg{fill:rgb(255, 255, 255)}</style>
-                <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"/>
-            </svg>
-            <!-- <span style="font-size: 0.75em;">Abstract</span> -->
-            </a>`}
-          <!--dark mode-->
-          <a class="nav-link ar5iv-toggle-color-scheme" href="javascript:toggleColorScheme()"
-            title="Toggle ar5iv color scheme" style="padding: 0.6rem;margin-top: 0rem;">
-            <span class="color-scheme-icon"></span>
-          </a>
-      </div>
-  </div>
+        <img alt="logo" class="logo" role="presentation" width="100" src="https://services.dev.arxiv.org/html/static/arxiv-logo-one-color-white.svg">
+        <span class="sr-only">back to arXiv</span>
+      </a>
+    </div>
+
+    <!--TOC, dark mode, links-->
+    <div class='html-header-nav'>
+      <button class="navbar-toggler ar5iv-footer-button" type="button" data-bs-theme="dark" data-bs-toggle="collapse" aria-expanded="false"
+        data-bs-target=".ltx_page_main >.ltx_TOC.mobile" aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation" style="border:none; margin-right: 0em;">
+        <span class="navbar-toggler-icon" style="width:1em;height:1em;margin-top: 0.1em;"></span>
+      </button>
+
+      <!--back to abstract-->
+      ${id === 'submission' ? '' : `
+        <a class="nav-link ar5iv-footer-button hover-effect" href="https://arxiv.org/abs/${window.location.href.match(/https:\/\/.+\/html\/(.+)/)[1]}">
+        <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512">
+            <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"/>
+        </svg>
+        </a>`}
+      <!--dark mode-->
+      <a class="ar5iv-toggle-color-scheme" href="javascript:toggleColorScheme()"
+        title="Toggle dark/light mode">
+        <span class="color-scheme-icon"></span>
+      </a>
+    </div>
     `;
     mob_header.innerHTML=mobile_header
-    mob_header.classList.add('navbar');
-    mob_header.classList.add('bg-body-tertiary');
+    // mob_header.classList.add('navbar');
+    // mob_header.classList.add('bg-body-tertiary');
     mob_header.classList.add('mob_header');
     document.body.insertBefore(mob_header, document.body.firstChild);
 }
