3ab18c450cb9bfde8e0e8c5ffdde19bf478cb4f0
Mark Nazzaro <marknazzaro2@gmail.com>
2024-01-30 09:22:49 -0500

    fix tz

--- browse/controllers/abs_page.py
+++ browse/controllers/abs_page.py
@@ -6,7 +6,7 @@ handles GET requests to the abs endpoint.
 # pylint: disable=raise-missing-from
 
 import re
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Any, Dict, List, Optional, Tuple
 from urllib.parse import urljoin
 
@@ -239,7 +239,7 @@ def _check_request_headers(
     docmeta: DocMetadata, response_data: Dict[str, Any], resp_headers: Dict[str, Any]
 ) -> bool:
     """Check the request headers, update the response headers accordingly."""
-    html_updated = get_latexml_publish_dt(docmeta.arxiv_id, docmeta.get_version()) or datetime.fromtimestamp(0)
+    html_updated = get_latexml_publish_dt(docmeta.arxiv_id, docmeta.get_version()) or datetime.fromtimestamp(0, tz=timezone.utc)
     last_mod_dt: datetime = max(html_updated, docmeta.modified)
 
     # Latest trackback ping time depends on the database
