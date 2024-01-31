0611f2955b0cbb485a009cb2e0dec0db90628a1f
Mark Nazzaro <marknazzaro2@gmail.com>
2024-01-31 12:50:09 -0500

    Unwrap optional differently to convince mypy this is ok

--- browse/controllers/abs_page.py
+++ browse/controllers/abs_page.py
@@ -239,8 +239,10 @@ def _check_request_headers(
     docmeta: DocMetadata, response_data: Dict[str, Any], resp_headers: Dict[str, Any]
 ) -> bool:
     """Check the request headers, update the response headers accordingly."""
-    html_updated = get_latexml_publish_dt(docmeta.arxiv_id, docmeta.get_version().version) or datetime.fromtimestamp(0, tz=timezone.utc) \
-        if docmeta.get_version() else datetime.fromtimestamp(0, tz=timezone.utc)
+    if docmeta.get_version():
+        html_updated = get_latexml_publish_dt(docmeta.arxiv_id, docmeta.get_version().version) or datetime.fromtimestamp(0, tz=timezone.utc)
+    else:
+        html_updated = datetime.fromtimestamp(0, tz=timezone.utc)
     last_mod_dt: datetime = max(html_updated, docmeta.modified)
 
     # Latest trackback ping time depends on the database
