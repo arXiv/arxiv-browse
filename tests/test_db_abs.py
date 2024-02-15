95453fb38040f30409318823c809195626a1da12
Brian D. Caruso <bdc34@cornell.edu>
2024-02-15 13:26:06 -0500

    Revert "Changing to using last-modified to etag on /abs arxivce-1247"
    This reverts commit 41a58bef1eb4304fb4fd89b387a75dbddc564908.

--- tests/test_db_abs.py
+++ tests/test_db_abs.py
@@ -72,28 +72,48 @@ def test_db_abs_null_source_size(dbclient):
 
 def test_html_conversion_dissemination (dbclient):
     rt = dbclient.get('/abs/0906.2112')
+
     assert rt.status_code == 200
+
+    assert rt.headers['Last-Modified'] == 'Mon, 01 Jan 2024 00:00:00 GMT'
+
     html = BeautifulSoup(rt.data.decode('utf-8'), 'html.parser')
     atag = html.select_one('.extra-services').find('a', {'id': 'latexml-download-link'})
+
     assert atag and atag.text == "HTML (experimental)"
 
-def test_old_html_conversion (dbclient):
+def test_last_modified_old_html_conversion (dbclient):
     rt = dbclient.get('/abs/2310.08262')
+
     assert rt.status_code == 200
+
+    assert rt.headers['Last-Modified'].startswith('Fri, 13 Oct 2023')
+
     html = BeautifulSoup(rt.data.decode('utf-8'), 'html.parser')
     atag = html.select_one('.extra-services').find('a', {'id': 'latexml-download-link'})
+
     assert atag and atag.text == "HTML (experimental)"
 
 def test_last_modified_no_publish_dt (dbclient):
     rt = dbclient.get('/abs/0906.5132')
+
     assert rt.status_code == 200
+
+    assert rt.headers['Last-Modified'].startswith('Thu, 15 Oct 2009')
+
     html = BeautifulSoup(rt.data.decode('utf-8'), 'html.parser')
     atag = html.select_one('.extra-services').find('a', {'id': 'latexml-download-link'})
+
     assert atag and atag.text == "HTML (experimental)"
 
 def test_last_modified_no_html (dbclient):
     rt = dbclient.get('/abs/0906.5504')
+    
     assert rt.status_code == 200
+
+    assert rt.headers['Last-Modified'].startswith('Tue, 13 Oct 2009')
+
     html = BeautifulSoup(rt.data.decode('utf-8'), 'html.parser')
     atag = html.select_one('.extra-services').find('a', {'id': 'latexml-download-link'})
+
     assert atag is None
