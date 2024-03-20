604aa3b286a3e9b1c179e6145c6d1861228324c7
Brian D. Caruso <bdc34@cornell.edu>
2024-03-20 15:34:27 -0400

    Adds tests for html sourced paper cs/9904010 ARXIVCE-1360

--- tests/test_html.py
+++ tests/test_html.py
@@ -31,3 +31,40 @@ def test_html_paper_multi_files(client_with_test_fs):
     resp = client_with_test_fs.get("/html/cs/9901011/timeline_v3.gif")
     assert resp.status_code == 200
     assert "gif" in resp.headers["content-type"]
+
+
+def test_html_paper_multi_html_files(client_with_test_fs):
+    """Test a paper with html source."""
+    resp = client_with_test_fs.head("/abs/cs/9904010")
+    assert resp.status_code == 200
+
+    resp = client_with_test_fs.get("/html/cs/9904010/shouldnotexist.html")
+    assert resp.status_code == 404
+
+    resp = client_with_test_fs.get("/html/cs/9904010")
+    assert resp.status_code == 200
+    assert "html" in resp.headers["content-type"]
+    txt = resp.data.decode()
+    assert "The following files are available for cs/9904010" in txt
+    assert "appendix.htm" in txt
+    assert "report.htm" in txt
+    assert "graph0.gif" not in txt
+    assert "graph1.gif" not in txt
+    assert "graph2.gif" not in txt
+    assert "graph3.gif" not in txt
+    assert "graph4.gif" not in txt
+    assert "graph5.gif" not in txt
+
+    resp = client_with_test_fs.get("/html/cs/9904010/report.htm")
+    assert resp.status_code == 200
+    assert b"Sample Characteristics and Response Rate" in resp.data
+    assert "html" in resp.headers["content-type"]
+
+    resp = client_with_test_fs.get("/html/cs/9904010/appendix.htm")
+    assert resp.status_code == 200
+    assert b"Appendix: Survey Questions and Frequency of Responses" in resp.data
+    assert "html" in resp.headers["content-type"]
+
+    resp = client_with_test_fs.get("/html/cs/9904010/graph1.gif")
+    assert resp.status_code == 200
+    assert "gif" in resp.headers["content-type"]
