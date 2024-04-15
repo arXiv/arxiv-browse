e4a41d7e9b4588a3bb1e147c6e220b64c2faeae9
kyokukou <erin@arxiv.org>
2024-04-15 09:51:56 -0700

    simplify fetching the return format

--- browse/services/dissemination/article_store.py
+++ browse/services/dissemination/article_store.py
@@ -243,7 +243,7 @@ class ArticleStore():
         if not fileobj:
             return "UNAVAILABLE"
         if isinstance(fileobj, FileObj):
-            return (fileobj, self.sourcestore.get_src_format(docmeta, fileobj), docmeta, version) #TODO I dont think we want to always return the source format
+            return (fileobj, self.sourcestore.get_return_format(fileobj), docmeta, version) 
         if isinstance(fileobj, List): #html requests return an iterable of files in the folder
             return (fileobj, format, docmeta, version) #type: ignore
         else:
