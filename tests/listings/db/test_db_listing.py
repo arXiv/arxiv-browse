c983d159b308a142270fa583ab3eb62adec849f2
kyokukou <erin@arxiv.org>
2024-06-10 12:10:57 -0700

    make sure ps link is gone

--- tests/listings/db/test_db_listing.py
+++ tests/listings/db/test_db_listing.py
@@ -49,6 +49,7 @@ def test_list_dl_links(client_with_db_listings):
     assert rv.status_code == 200
     assert '<a href="/pdf/0906.3421" title="Download PDF" id="pdf-0906.3421" aria-labelledby="pdf-0906.3421">pdf</a>' in rv.text
     assert '<a href="/format/0906.3421" title="Other formats" id="oth-0906.3421" aria-labelledby="oth-0906.3421">other</a>' in rv.text
+    assert '<a href="/ps/0906.3421" title="Download PostScript" id="ps-0906.3421" aria-labelledby="ps-0906.3421">ps</a>' not in rv.text
 
 def test_possible_categories():
     
