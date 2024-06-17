9dda9c82ca6877590008ba2bb0e32d93a3eb49b0
kyokukou <erin@arxiv.org>
2024-06-17 08:12:58 -0700

    pdf specific key updates

--- tests/dissemination/test_pdf.py
+++ tests/dissemination/test_pdf.py
@@ -1,18 +1,16 @@
 
 
 def test_pdf_headers(client_with_test_fs):
-    #client=client_with_test_fs
-
     rv=client_with_test_fs.head("/pdf/cs/0011004")
     head=rv.headers["Surrogate-Key"]
     assert " pdf " in " "+head+" "
-    assert "pdf-unversioned" in head
-    assert "pdf-versioned" not in head
+    assert "pdf-cs/0011004-current" in head
+    assert "pdf-cs/0011004v" not in head
     assert "paper-id-cs/0011004" in head
 
     rv=client_with_test_fs.head("/pdf/cs/0011004v1")
     head=rv.headers["Surrogate-Key"]
     assert " pdf " in " "+head+" "
-    assert "pdf-unversioned" not in head
-    assert "pdf-versioned" in head
+    assert "pdf-cs/0011004-current" not in head
+    assert "pdf-cs/0011004v1" in head
     assert "paper-id-cs/0011004" in head
