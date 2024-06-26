2a029829e64b875d99bea4962eea3488337676de
Jake Weiskoff <jweiskoff+github@gmail.com>
2024-06-26 11:19:33 -0500

    fixes formatting again.

--- browse/formatting/cite.py
+++ browse/formatting/cite.py
@@ -18,7 +18,7 @@ def arxiv_bibtex(docm: DocMetadata) -> str:
     auths = _fmt_author_list(pauths)
 
     pc = docm.primary_category.id if docm.primary_category else "unknown"
-    doi = "     doi={https://doi.org/" + docm.doi + "}\n" if docm.doi else ""
+    doi = f"      doi={https://doi.org/{docm.doi}},\n" if docm.doi else ""
 
     return (
         "@misc{" + _txt_id(docm, pauths, year) + ",\n"
