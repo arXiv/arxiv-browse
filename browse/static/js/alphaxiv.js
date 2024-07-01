6d6f9b9a83847e2fad73a56b60182b17aa8deb8d
rehaanahmad2013 <rehaanahmad2013@gmail.com>
2024-06-30 23:04:44 -0700

    small updates

--- browse/static/js/alphaxiv.js
+++ browse/static/js/alphaxiv.js
@@ -58,17 +58,16 @@
         let resultStr = ""
         if (numComments == 0) {
             resultStr += `<h3 class="alphaxiv-summary">Comment on arXiv papers</h3>
-            <p> No comments for this paper. View recent comments on other papers <a href="https://alphaxiv.org/explore" target="_blank">here</a>.</p>`
+            <p> No comments for this paper. View recent comments <a href="https://alphaxiv.org/explore" target="_blank">on other papers here</a>.</p>`
         } else if (numComments == 1) {
             resultStr += `<h3 class="alphaxiv-summary">There is 1 comment on this paper</h3>
             <p> View comments on <a href="${alphaXivUrl}" target="_blank">alphaXiv</a> and add your own!</p>`
         } else {
-            resultStr += `<h3 class="alpahxiv-summary">There are ${conversations} comments for this paper on alphaXiv</h3>
+            resultStr += `<h3 class="alpahxiv-summary">There are ${numComments} comments for this paper on alphaXiv</h3>
             <p> View comments on <a href="${alphaXivUrl}" target="_blank">alphaXiv</a>.</p>`
         }
         if (hasClaimedAuthorship) {
-            resultStr += `<h3 class="alphaxiv-summary">Author Present On alphaXiv</h3>
-            <p> Author of paper is on alphaXiv and will be notified of yours comments: see <a href="${alphaXivUrl}" target="_blank">here</a>.</p>`
+            resultStr += `<p> For this paper, the author is present on alphaXiv and will be notified of yours comments: see <a href="${alphaXivUrl}" target="_blank">here</a>.</p>`
         }
 
         return resultStr
