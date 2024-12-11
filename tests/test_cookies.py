a218326afea9e042f74a090dc52b79dfdab76793
deyan <deyan.ginev@gmail.com>
2024-12-11 16:13:17 -0500

    [remove test] no longer using cookies for preferred format

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -6,7 +6,6 @@ def test_cookies_with_no_params(client_with_test_fs):
     rv = client_with_test_fs.get('/cookies')
     assert rv.status_code == 200
     html = rv.data.decode('utf-8')
-    assert 'Select preferred download format' in html
     assert 'show additional debugging information' in html, 'should have SHOW debugging link'
 
 def test_cookies_with_debug(client_with_test_fs):
@@ -14,5 +13,4 @@ def test_cookies_with_debug(client_with_test_fs):
     rv = client_with_test_fs.get('/cookies?debug=1')
     assert rv.status_code == 200
     html = rv.data.decode('utf-8')
-    assert 'Select preferred download format' in html
     assert 'hide debugging information' in html, 'should have HIDE debugging link'
