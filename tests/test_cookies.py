36e3fe7d68eb176ddb6ead1dde4741c1dcc87d00
deyan <deyan.ginev@gmail.com>
2024-12-11 16:06:43 -0500

    remove only /set test, keep the GET tests

---
+++ tests/test_cookies.py
@@ -0,0 +1,18 @@
+"""test cookies"""
+
+
+def test_cookies_with_no_params(client_with_test_fs):
+    """Test the cookies page."""
+    rv = client_with_test_fs.get('/cookies')
+    assert rv.status_code == 200
+    html = rv.data.decode('utf-8')
+    assert 'Select preferred download format' in html
+    assert 'show additional debugging information' in html, 'should have SHOW debugging link'
+
+def test_cookies_with_debug(client_with_test_fs):
+    """Test the cookies page."""
+    rv = client_with_test_fs.get('/cookies?debug=1')
+    assert rv.status_code == 200
+    html = rv.data.decode('utf-8')
+    assert 'Select preferred download format' in html
+    assert 'hide debugging information' in html, 'should have HIDE debugging link'
