5b0e999311312f1dad38de4a1501ef5a6cad1d10
Brian Maltzan <bgm37@cornell.edu>
2026-08-07 16:00:57 -0400

    billing labels: add placeholder values

--- cicd/deploy/main.tf
+++ cicd/deploy/main.tf
@@ -50,6 +50,11 @@ resource "google_cloud_run_v2_service" "arxiv_browse" {
   name     = "arxiv-browse"
   location = var.region
 
+  labels = {
+    "arxiv-system"    = "dissemination"
+    "arxiv-subsystem" = "dis-browse"
+  }
+
   depends_on = [
     google_project_iam_member.browse_secret_accessor,
     google_project_iam_member.browse_sa_cloudsql_client,
